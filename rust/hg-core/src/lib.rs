97d689c92254ba787a3ad531b1d59cbb789c4ed3
Georges Racinet <gracinet@anybox.fr>
2018-12-04 11:05:06 +0100

    rust: core implementation for lazyancestors
    Once exposed through appropriate bindings, this
    should be able to replace ancestor.lazyancestors
    entirely.
    
    Differential Revision: https://phab.mercurial-scm.org/D5440

--- rust/hg-core/src/lib.rs
+++ rust/hg-core/src/lib.rs
@@ -3,7 +3,7 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
 mod ancestors;
-pub use ancestors::{AncestorsIterator, MissingAncestors};
+pub use ancestors::{AncestorsIterator, LazyAncestors, MissingAncestors};
 
 /// Mercurial revision numbers
 ///
