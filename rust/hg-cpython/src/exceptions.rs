879684c3eb8da3a04b9f29ce46d9c165e3fff754
Georges Racinet <georges.racinet@octobus.net>
2018-12-22 11:38:03 +0100

    rust-cpython: rustdoc improvements
    By default, `cargo doc` builds the documentation for public
    constructs only, so we make public those that can. Since `cindex`
    is not safe, we keep it private.
    
    Unfortunately, the macro syntax of rust-cpython doesn't allow us
    to document the classes directly, so we resort to do that at
    the module level.
    
    Differential Revision: https://phab.mercurial-scm.org/D5546

--- rust/hg-cpython/src/exceptions.rs
+++ rust/hg-cpython/src/exceptions.rs
@@ -1,3 +1,15 @@
+// ancestors.rs
+//
+// Copyright 2018 Georges Racinet <gracinet@anybox.fr>
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 or any later version.
+
+//! Bindings for Rust errors
+//!
+//! [`GraphError`] exposes `hg::GraphError` as a subclass of `ValueError`
+//!
+//! [`GraphError`]: struct.GraphError.html
 use cpython::exc::ValueError;
 use cpython::{PyErr, Python};
 use hg;
