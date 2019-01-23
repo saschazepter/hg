33499a42c3d3c7e3fa1b7a2711ce8e326dd485e9
Gregory Szorc <gregory.szorc@gmail.com>
2019-01-22 18:25:34 -0800

    remotefilelog: use %d to format an int
    Differential Revision: https://phab.mercurial-scm.org/D5656

--- hgext/remotefilelog/basepack.py
+++ hgext/remotefilelog/basepack.py
@@ -270,9 +270,9 @@ class versionmixin(object):
                 # only affect this instance
                 self.VERSION = version
             elif self.VERSION != version:
-                raise RuntimeError('inconsistent version: %s' % version)
+                raise RuntimeError('inconsistent version: %d' % version)
         else:
-            raise RuntimeError('unsupported version: %s' % version)
+            raise RuntimeError('unsupported version: %d' % version)
 
 class basepack(versionmixin):
     # The maximum amount we should read via mmap before remmaping so the old
