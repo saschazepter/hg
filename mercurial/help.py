7f4882d770b41808e389a09cbf157d43ed2921eb
Pierre-Yves David <pierre-yves.david@octobus.net>
2019-03-27 16:45:14 +0100

    util: extract compression code in `mercurial.utils.compression`
    The code seems large enough to be worth extracting. This is similar to what was
    done for various module in `mercurial/utils/`.
    
    Since None of the compression logic takes a `ui` objet, issuing deprecation
    warning is tricky. Luckly the logic does not seems to have many external users.

--- mercurial/help.py
+++ mercurial/help.py
@@ -37,6 +37,9 @@ from . import (
 from .hgweb import (
     webcommands,
 )
+from .utils import (
+    compression,
+)
 
 _exclkeywords = {
     "(ADVANCED)",
@@ -428,7 +431,7 @@ def addtopicsymbols(topic, marker, symbols, dedent=False):
     addtopichook(topic, add)
 
 addtopicsymbols('bundlespec', '.. bundlecompressionmarker',
-                util.bundlecompressiontopics())
+                compression.bundlecompressiontopics())
 addtopicsymbols('filesets', '.. predicatesmarker', fileset.symbols)
 addtopicsymbols('merge-tools', '.. internaltoolsmarker',
                 filemerge.internalsdoc)
