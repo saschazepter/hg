87247597c8a6db30385b9d4771e037078b6ceb3a
Martin von Zweigbergk <martinvonz@google.com>
2019-05-03 08:37:10 -0700

    localrepo: don't use defaults arguments that will never be overridden
    The commithook() callback will be called when the lock is
    released. lock.release() calls the callback without arguments, so it
    was quite confusing to me that this function declared extra
    arguments. We can just close on the variables in the outer scope
    instead.
    
    Differential Revision: https://phab.mercurial-scm.org/D6336

--- mercurial/localrepo.py
+++ mercurial/localrepo.py
@@ -2538,12 +2538,12 @@ class localrepository(object):
                         _('note: commit message saved in %s\n') % msgfn)
                 raise
 
-        def commithook(node=hex(ret), parent1=hookp1, parent2=hookp2):
+        def commithook():
             # hack for command that use a temporary commit (eg: histedit)
             # temporary commit got stripped before hook release
             if self.changelog.hasnode(ret):
-                self.hook("commit", node=node, parent1=parent1,
-                          parent2=parent2)
+                self.hook("commit", node=hex(ret), parent1=hookp1,
+                          parent2=hookp2)
         self._afterlock(commithook)
         return ret
 
