767ffb2576f4122a74d820b31854d11305075cd5
Raphaël Gomès <rgomes@octobus.net>
2019-06-05 17:58:34 +0200

    rust-dirstate: call new "dirs" rust implementation from Python
    This is a simple module attribute replacement, will take precedence over the
    Python and C implementations.
    
    Differential Revision: https://phab.mercurial-scm.org/D6395
    
    --HG--
    extra : amend_source : f5ef8c85477d3add09dbf6f3210209bab41d8639

--- mercurial/util.py
+++ mercurial/util.py
@@ -53,6 +53,8 @@ from .utils import (
     stringutil,
 )
 
+rustdirs = policy.importrust('dirstate', 'Dirs')
+
 base85 = policy.importmod(r'base85')
 osutil = policy.importmod(r'osutil')
 parsers = policy.importmod(r'parsers')
@@ -3203,6 +3205,9 @@ class dirs(object):
 
 if safehasattr(parsers, 'dirs'):
     dirs = parsers.dirs
+
+if rustdirs is not None:
+    dirs = rustdirs
 
 def finddirs(path):
     pos = path.rfind('/')
