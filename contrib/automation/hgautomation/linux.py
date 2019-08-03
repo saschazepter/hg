21603ba22487e59bfa8fdbcc5881cee7060382b1
Gregory Szorc <gregory.szorc@gmail.com>
2019-08-03 14:04:31 -0700

    automation: install latest Python versions
    This required bumping the pyenv commit so the new versions are
    available.
    
    Differential Revision: https://phab.mercurial-scm.org/D6713
    
    --HG--
    extra : rebase_source : 76d5189b7c8badb171483d3dd8991d6101ff3e3e

--- contrib/automation/hgautomation/linux.py
+++ contrib/automation/hgautomation/linux.py
@@ -28,11 +28,11 @@ DISTROS = {
 
 INSTALL_PYTHONS = r'''
 PYENV2_VERSIONS="2.7.16 pypy2.7-7.1.1"
-PYENV3_VERSIONS="3.5.7 3.6.8 3.7.3 3.8-dev pypy3.5-7.0.0 pypy3.6-7.1.1"
+PYENV3_VERSIONS="3.5.7 3.6.9 3.7.4 3.8-dev pypy3.5-7.0.0 pypy3.6-7.1.1"
 
 git clone https://github.com/pyenv/pyenv.git /hgdev/pyenv
 pushd /hgdev/pyenv
-git checkout 3faeda67bb33e07750d1a104271369a7384ca45c
+git checkout 17f44b7cd6f58ea2fa68ec0371fb9e7a826b8be2
 popd
 
 export PYENV_ROOT="/hgdev/pyenv"
