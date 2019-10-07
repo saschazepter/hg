99b2086f1680a0d3267b5e973901fb94496bd7ff
Raphaël Gomès <rgomes@octobus.net>
2019-10-07 23:17:44 +0200

    rust-utils: introduce a debug util to print the python stack trace
    Differential Revision: https://phab.mercurial-scm.org/D7057
    
    --HG--
    extra : absorb_source : 886f0822a38e222dbe7097b86bcc428492e564c5
    extra : histedit_source : 8a6df4e4d23649c76c2fa093ff076a96e75fff52
    extra : rebase_source : ba156ab3e79200ef58a7c8e1b22c408b630d1616

--- rust/hg-cpython/src/lib.rs
+++ rust/hg-cpython/src/lib.rs
@@ -35,6 +35,7 @@ pub mod discovery;
 pub mod exceptions;
 pub mod filepatterns;
 pub mod parsers;
+pub mod utils;
 
 py_module_initializer!(rustext, initrustext, PyInit_rustext, |py, m| {
     m.add(
