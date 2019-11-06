a085ac8e697cb0f2bae578566ec245420d91c9b0
Pierre-Yves David <pierre-yves.david@octobus.net>
2019-11-06 14:13:19 +0100

    revlog: introduce an explicit NodeMap class for pure code
    This class make the "pure" nodemap raise the same exception than the C-extension
    one.
    
    This is a step toward unifying nodemap and index, the class is not meant to
    survive on the long run.
    
    This work is part of a refactoring to unify the revlog index and the nodemap.
    This unification prepare the use of a persistent nodemap.
    
    There is a new `isinstance` call, it will be cleaned up in coming changesets.
    
    Differential Revision: https://phab.mercurial-scm.org/D7312

---
+++ mercurial/revlogutils/__init__.py
@@ -0,0 +1,14 @@
+# mercurial.revlogutils -- basic utilities for revlog
+#
+# Copyright 2019 Pierre-Yves David <pierre-yves.david@octobus.net>
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+from __future__ import absolute_import
+from .. import error
+
+
+class NodeMap(dict):
+    def __missing__(self, x):
+        raise error.RevlogError('unknown node: %s' % x)
