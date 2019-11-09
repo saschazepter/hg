5f9b33549a893ff6994d776cf3c3e11f59441054
Pierre-Yves David <pierre-yves.david@octobus.net>
2019-11-09 13:23:51 +0100

    index: add a `rev` method (API)
    The new `index.rev(node)` is to be preferred over using `node
    index.nodemap[node]`.
    
    This get us closer to be able to remove the `nodemap` attribute of the index.
    
    Differential Revision: https://phab.mercurial-scm.org/D7324

--- mercurial/cext/parsers.c
+++ mercurial/cext/parsers.c
@@ -667,7 +667,7 @@ void dirs_module_init(PyObject *mod);
 void manifest_module_init(PyObject *mod);
 void revlog_module_init(PyObject *mod);
 
-static const int version = 14;
+static const int version = 15;
 
 static void module_init(PyObject *mod)
 {
