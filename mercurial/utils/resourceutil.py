0b895559456006f5cc4c546d75661e34b69f11e6
Martin von Zweigbergk <martinvonz@google.com>
2019-11-14 12:33:10 -0800

    util: move definition of datapath to resourceutil
    Since this means moving the function into a subdirectory, we have to
    compensate by adding another layer of os.path.dirname().
    
    Differential Revision: https://phab.mercurial-scm.org/D7434
    
    --HG--
    extra : absorb_source : 4405d885825918d536d3028e4c7c788459727dcc
    extra : histedit_source : 259dec8ed1b46bae550aca6077fdf1ef79ddacf7

--- mercurial/utils/resourceutil.py
+++ mercurial/utils/resourceutil.py
@@ -10,6 +10,7 @@
 from __future__ import absolute_import
 
 import imp
+import os
 import sys
 
 from .. import pycompat
@@ -26,3 +27,11 @@ def mainfrozen():
         or pycompat.safehasattr(sys, "importers")  # new py2exe
         or imp.is_frozen("__main__")  # old py2exe
     )  # tools/freeze
+
+
+# the location of data files matching the source code
+if mainfrozen() and getattr(sys, 'frozen', None) != 'macosx_app':
+    # executable version (py2exe) doesn't support __file__
+    datapath = os.path.dirname(pycompat.sysexecutable)
+else:
+    datapath = os.path.dirname(os.path.dirname(pycompat.fsencode(__file__)))
