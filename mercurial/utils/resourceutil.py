9e01cf495698859166d717cf5a22cc03cb5d20b9
Martin von Zweigbergk <martinvonz@google.com>
2019-11-14 11:52:22 -0800

    procutil: move mainfrozen() to new resourceutil.py
    The i18n module practically depends on procutil for mainfrozen() but
    since procutil depends on i18n, it would be a circular dependency if
    i18n depended directly on procutil. The cycle is currently resolved by
    having the higher-level util module calculate the "datapath" and
    inject it into i18n. Extracting mainfrozen() to a new module lets us
    clean up the dependencies.
    
    Differential Revision: https://phab.mercurial-scm.org/D7433

---
+++ mercurial/utils/resourceutil.py
@@ -0,0 +1,28 @@
+# resourceutil.py - utility for looking up resources
+#
+#  Copyright 2005 K. Thananchayan <thananck@yahoo.com>
+#  Copyright 2005-2007 Matt Mackall <mpm@selenic.com>
+#  Copyright 2006 Vadim Gelfer <vadim.gelfer@gmail.com>
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+from __future__ import absolute_import
+
+import imp
+import sys
+
+from .. import pycompat
+
+
+def mainfrozen():
+    """return True if we are a frozen executable.
+
+    The code supports py2exe (most common, Windows only) and tools/freeze
+    (portable, not much used).
+    """
+    return (
+        pycompat.safehasattr(sys, "frozen")
+        or pycompat.safehasattr(sys, "importers")  # new py2exe
+        or imp.is_frozen("__main__")  # old py2exe
+    )  # tools/freeze
