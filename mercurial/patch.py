e47ed4098a78e1079e08877073abfd40a826e48e
Matt Harbison <matt_harbison@yahoo.com>
2019-12-16 23:27:17 -0500

    patch: make __repr__() return str
    Caught by pytype:
    
        line 969, in __repr__: Function bytes.join was called with the wrong arguments [wrong-arg-types]
        Expected: (self, iterable: Iterable[bytes])
        Actually passed: (self, iterable: Iterator[str])
    
    Differential Revision: https://phab.mercurial-scm.org/D7682

--- mercurial/patch.py
+++ mercurial/patch.py
@@ -963,7 +963,7 @@ class header(object):
         return self.files()[-1]
 
     def __repr__(self):
-        return b'<header %s>' % (b' '.join(map(repr, self.files())))
+        return '<header %s>' % (' '.join(map(repr, self.files())))
 
     def isnewfile(self):
         return any(self.newfile_re.match(h) for h in self.header)
