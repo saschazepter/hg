adf4c511a846a29cdd5a1a9b881a616076eb03f5
Yuya Nishihara <yuya@tcha.org>
2020-04-10 22:07:11 +0900

    rust-chg: reimplement attach_io operation as async function
    In short, MessageLoop<Connection> was redesigned as Protocol<Connection>,
    and the protocol methods no longer consume self.
    
    API changes are briefly documented in the following page:
    https://docs.rs/tokio-hglib/0.3.0/tokio_hglib/struct.Protocol.html
    
    Differential Revision: https://phab.mercurial-scm.org/D8442

--- rust/chg/src/lib.rs
+++ rust/chg/src/lib.rs
@@ -3,7 +3,7 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
 
-//mod attachio;
+mod attachio;
 //mod clientext;
 //pub mod locator;
 pub mod message;
