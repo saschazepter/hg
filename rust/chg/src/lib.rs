63e835ac450950074a3dd75936f75e1de8cb99c0
Yuya Nishihara <yuya@tcha.org>
2020-04-10 21:35:16 +0900

    rust-chg: upgrade to 2018 edition and remove useless extern crates
    Differential Revision: https://phab.mercurial-scm.org/D8399

--- rust/chg/src/lib.rs
+++ rust/chg/src/lib.rs
@@ -3,17 +3,6 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
 
-extern crate bytes;
-#[macro_use]
-extern crate futures;
-extern crate libc;
-#[macro_use]
-extern crate log;
-extern crate tokio;
-extern crate tokio_hglib;
-extern crate tokio_process;
-extern crate tokio_timer;
-
 mod attachio;
 mod clientext;
 pub mod locator;
