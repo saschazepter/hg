7550c8458acb6bf2eacb56521367afa60aaa07fe
Sushil khanchi <sushilkhanchi97@gmail.com>
2020-06-09 22:02:09 +0530

    phabricator: make it clear what happen when no response
    Differential Revision: https://phab.mercurial-scm.org/D8621

--- hgext/phabricator.py
+++ hgext/phabricator.py
@@ -1650,7 +1650,7 @@ def _confirmbeforesend(repo, revs, oldmap):
         )
 
     if ui.promptchoice(
-        _(b'Send the above changes to %s (yn)?$$ &Yes $$ &No') % url
+        _(b'Send the above changes to %s (Y/n)?$$ &Yes $$ &No') % url
     ):
         return False
 
