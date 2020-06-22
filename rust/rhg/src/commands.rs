0004850982f6da963c8d8248b0076fcd539d5094
Antoine Cezar <antoine.cezar@octobus.net>
2020-06-22 15:19:35 +0530

    rhg: add Command trait for subcommands implemented by rhg
    Normalizes the interface of the cli's commands
    
    Differential Revision: https://phab.mercurial-scm.org/D8611
    
    Differential Revision: https://phab.mercurial-scm.org/D8648

---
+++ rust/rhg/src/commands.rs
@@ -0,0 +1,8 @@
+use crate::error::CommandError;
+
+/// The common trait for rhg commands
+///
+/// Normalize the interface of the commands provided by rhg
+pub trait Command {
+    fn run(&self) -> Result<(), CommandError>;
+}
