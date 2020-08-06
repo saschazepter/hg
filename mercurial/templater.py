saschazepter/hg

commit 30ecbda881d23d249ad0e00de68c016f9aa70b6d

templater: make open_template() read from resources if in frozen binary
This takes the number of failing tests with PyOxidizer from 87 to 72.

Differential Revision: https://phab.mercurial-scm.org/D8894

mercurial/templater.py
@@ -1074,7 +1074,12 @@ def templatedir():
 
 
 def open_template(name):
-    '''returns a file-like object for the given template, and its full path'''
+    '''returns a file-like object for the given template, and its full path
+
+    If the name is a relative path and we're in a frozen binary, the template
+    will be read from the mercurial.templates package instead. The returned path
+    will then be the relative path.
+    '''
     templatepath = templatedir()
     if templatepath is not None or os.path.isabs(name):
         f = os.path.join(templatepath, name)
@@ -1083,5 +1088,12 @@ def open_template(name):
         except EnvironmentError:
             return None, None
     else:
-        # TODO: read from resources here
-        return None, None
+        name_parts = pycompat.sysstr(name).split('/')
+        package_name = '.'.join(['mercurial', 'templates'] + name_parts[:-1])
+        try:
+            return (
+                name,
+                resourceutil.open_resource(package_name, name_parts[-1]),
+            )
+        except (ModuleNotFoundError, FileNotFoundError):
+            return None, None

commit 0e7553450516b3effcc0a3bbb8d8ae4db9e81cc5

templater: replace Py3-only exception types by super-types available in Py2
As noted by @indygreg, `test-check-pyflakes.t` started failing on Py2
after my recent D8894, because that introduced catching of the
Py3-only types `ModuleNotFoundError` and `FileNotFoundError`. Let's
switch to less precise types that are also available in Py2.

Differential Revision: https://phab.mercurial-scm.org/D8902

mercurial/templater.py
@@ -1107,5 +1107,5 @@ def open_template(name, templatepath=None):
                 name,
                 resourceutil.open_resource(package_name, name_parts[-1]),
             )
-        except (ModuleNotFoundError, FileNotFoundError):
+        except (ImportError, OSError):
             return None, None