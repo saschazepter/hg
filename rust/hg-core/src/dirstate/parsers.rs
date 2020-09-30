ef84e16d656779ea0b58da40b2cbd8d7985fd776
Raphaël Gomès <rgomes@octobus.net>
2020-09-30 18:10:29 +0200

    rust: start plugging the dirstate tree behind a feature gate
    The previous patch added the `dirstate-tree` feature gate to enable the two
    dirstate implementations to co-habit while the tree-based one gets better.
    
    This patch copies over the code that differs, be it because the algorithm
    changed or because the borrowing rules are different.
    
    Indeed, `DirstateTree` is not observationally equivalent to the std `HashMap` in
    the APIs we use: it does not have the `Entry` API (yet?) and its iterator
    returns owned values instead of references. This last point is because the
    implementation needs to be changed to a more clever and efficient solution.
    
    Differential Revision: https://phab.mercurial-scm.org/D9133

--- rust/hg-core/src/dirstate/parsers.rs
+++ rust/hg-core/src/dirstate/parsers.rs
@@ -80,11 +80,11 @@ pub fn parse_dirstate(
         ));
         curr_pos = curr_pos + MIN_ENTRY_SIZE + (path_len);
     }
-
     Ok((parents, entries, copies))
 }
 
 /// `now` is the duration in seconds since the Unix epoch
+#[cfg(not(feature = "dirstate-tree"))]
 pub fn pack_dirstate(
     state_map: &mut StateMap,
     copy_map: &CopyMap,
@@ -156,15 +156,89 @@ pub fn pack_dirstate(
 
     Ok(packed)
 }
+/// `now` is the duration in seconds since the Unix epoch
+#[cfg(feature = "dirstate-tree")]
+pub fn pack_dirstate(
+    state_map: &mut StateMap,
+    copy_map: &CopyMap,
+    parents: DirstateParents,
+    now: Duration,
+) -> Result<Vec<u8>, DirstatePackError> {
+    // TODO move away from i32 before 2038.
+    let now: i32 = now.as_secs().try_into().expect("time overflow");
+
+    let expected_size: usize = state_map
+        .iter()
+        .map(|(filename, _)| {
+            let mut length = MIN_ENTRY_SIZE + filename.len();
+            if let Some(copy) = copy_map.get(&filename) {
+                length += copy.len() + 1;
+            }
+            length
+        })
+        .sum();
+    let expected_size = expected_size + PARENT_SIZE * 2;
+
+    let mut packed = Vec::with_capacity(expected_size);
+    let mut new_state_map = vec![];
+
+    packed.extend(&parents.p1);
+    packed.extend(&parents.p2);
+
+    for (filename, entry) in state_map.iter() {
+        let new_filename = filename.to_owned();
+        let mut new_mtime: i32 = entry.mtime;
+        if entry.state == EntryState::Normal && entry.mtime == now {
+            // The file was last modified "simultaneously" with the current
+            // write to dirstate (i.e. within the same second for file-
+            // systems with a granularity of 1 sec). This commonly happens
+            // for at least a couple of files on 'update'.
+            // The user could change the file without changing its size
+            // within the same second. Invalidate the file's mtime in
+            // dirstate, forcing future 'status' calls to compare the
+            // contents of the file if the size is the same. This prevents
+            // mistakenly treating such files as clean.
+            new_mtime = -1;
+            new_state_map.push((
+                filename.to_owned(),
+                DirstateEntry {
+                    mtime: new_mtime,
+                    ..entry
+                },
+            ));
+        }
+        let mut new_filename = new_filename.into_vec();
+        if let Some(copy) = copy_map.get(&filename) {
+            new_filename.push(b'\0');
+            new_filename.extend(copy.bytes());
+        }
+
+        packed.write_u8(entry.state.into())?;
+        packed.write_i32::<BigEndian>(entry.mode)?;
+        packed.write_i32::<BigEndian>(entry.size)?;
+        packed.write_i32::<BigEndian>(new_mtime)?;
+        packed.write_i32::<BigEndian>(new_filename.len() as i32)?;
+        packed.extend(new_filename)
+    }
+
+    if packed.len() != expected_size {
+        return Err(DirstatePackError::BadSize(expected_size, packed.len()));
+    }
+
+    state_map.extend(new_state_map);
+
+    Ok(packed)
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::{utils::hg_path::HgPathBuf, FastHashMap};
+    use pretty_assertions::assert_eq;
 
     #[test]
     fn test_pack_dirstate_empty() {
-        let mut state_map: StateMap = FastHashMap::default();
+        let mut state_map = StateMap::default();
         let copymap = FastHashMap::default();
         let parents = DirstateParents {
             p1: *b"12345678910111213141",
