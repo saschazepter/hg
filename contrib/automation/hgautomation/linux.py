51a0ca69ac4fd95bf4795b91106aad3130ea57e7
Gregory Szorc <gregory.szorc@gmail.com>
2020-10-08 18:07:34 -0700

    contrib: install Python 3.9.0
    We update the Windows and Linux environments to install CPython 3.9.0,
    which was released a few days ago.
    
    Differential Revision: https://phab.mercurial-scm.org/D9186

--- contrib/automation/hgautomation/linux.py
+++ contrib/automation/hgautomation/linux.py
@@ -26,11 +26,11 @@ DISTROS = {
 
 INSTALL_PYTHONS = r'''
 PYENV2_VERSIONS="2.7.17 pypy2.7-7.2.0"
-PYENV3_VERSIONS="3.5.10 3.6.12 3.7.9 3.8.6 pypy3.5-7.0.0 pypy3.6-7.3.0"
+PYENV3_VERSIONS="3.5.10 3.6.12 3.7.9 3.8.6 3.9.0 pypy3.5-7.0.0 pypy3.6-7.3.0"
 
 git clone https://github.com/pyenv/pyenv.git /hgdev/pyenv
 pushd /hgdev/pyenv
-git checkout 8bf79eb44ce1ea0a8b36544eb3e8a694a3a6ed78
+git checkout 8ac91b4fd678a8c04356f5ec85cfcd565c265e9a
 popd
 
 export PYENV_ROOT="/hgdev/pyenv"
