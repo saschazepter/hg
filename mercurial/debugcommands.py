91a1dc350f3751762027455119a4d32741ee9890
Matt Harbison <matt_harbison@yahoo.com>
2020-11-23 11:47:06 -0500

    ui: ensure `getpass()` returns bytes
    Previously, this could return either bytes or str.  I'm not sure which direction
    we should go in, but since the input is bytes, I guess bytes makes sense as
    output.  `debuguigetpass` crashed because it assumed bytes would be returned,
    `sslcontext.load_cert_chain()` is happy with bytes or str if the type info in
    PyCharm is correct, and `smtplib.SMTP.login()` wants str.
    
    I couldn't figure out how to test this, because the test stalls for input with
    `echo test | hg debuguigetpass --config ui.interactive=1`, likely because it
    drains stdin before prompting.  The custom input reading with `ui.nontty=1` does
    not.
    
    I'm also a bit concerned with all of this encoding/decoding.  The existing code
    in the mail module uses `encoding.strfromlocal()`, but the username and password
    are ascii encoded/decoded in `mercurial.url.passwordmgr.find_user_password()`
    with `pycompat.{str,bytes}url()`.  I'm not sure if this inconsistency could
    cause subtle compatability issues.
    
    Differential Revision: https://phab.mercurial-scm.org/D9375
    
    --HG--
    branch : stable
    extra : source : 87e7dd8e77346d80f7c41f7a5dfdd04e305c0048

--- mercurial/debugcommands.py
+++ mercurial/debugcommands.py
@@ -3787,6 +3787,10 @@ def debugtemplate(ui, repo, tmpl, **opts):
 def debuguigetpass(ui, prompt=b''):
     """show prompt to type password"""
     r = ui.getpass(prompt)
+    if r is not None:
+        r = encoding.strtolocal(r)
+    else:
+        r = b"<default response>"
     ui.writenoi18n(b'response: %s\n' % r)
 
 
