db5b3991524a7f6cb052add94e8cc05b0d889981
Matt Harbison <matt_harbison@yahoo.com>
2020-11-27 19:35:37 -0500

    formatting: drop a few extra double quotes in docstrings
    These were made obvious by the reformatting in D9430.
    
    Differential Revision: https://phab.mercurial-scm.org/D9432

--- mercurial/interfaces/repository.py
+++ mercurial/interfaces/repository.py
@@ -617,7 +617,7 @@ class ifiledata(interfaceutil.Interface):
         """
 
     def revision(node, raw=False):
-        """ "Obtain fulltext data for a node.
+        """Obtain fulltext data for a node.
 
         By default, any storage transformations are applied before the data
         is returned. If ``raw`` is True, non-raw storage transformations
