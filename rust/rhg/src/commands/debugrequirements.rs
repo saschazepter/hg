ad639847124aebd4fb3d2cfac5a4673aefb82259
Simon Sapin <simon.sapin@octobus.net>
2021-01-14 13:04:12 +0100

    rhg: initial support for shared repositories
    Differential Revision: https://phab.mercurial-scm.org/D9941

--- rust/rhg/src/commands/debugrequirements.rs
+++ rust/rhg/src/commands/debugrequirements.rs
@@ -2,7 +2,6 @@ use crate::commands::Command;
 use crate::error::CommandError;
 use crate::ui::Ui;
 use hg::repo::Repo;
-use hg::requirements;
 
 pub const HELP_TEXT: &str = "
 Print the current repo requirements.
@@ -20,8 +19,10 @@ impl Command for DebugRequirementsCommand {
     fn run(&self, ui: &Ui) -> Result<(), CommandError> {
         let repo = Repo::find()?;
         let mut output = String::new();
-        for req in requirements::load(&repo)? {
-            output.push_str(&req);
+        let mut requirements: Vec<_> = repo.requirements().iter().collect();
+        requirements.sort();
+        for req in requirements {
+            output.push_str(req);
             output.push('\n');
         }
         ui.write_stdout(output.as_bytes())?;
