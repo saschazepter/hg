3baeb6b4bd50617b9773169800f0b7106504fc54
Raphaël Gomès <rgomes@octobus.net>
2021-01-18 11:44:51 +0100

    sidedata: move to new sidedata storage in revlogv2
    The current (experimental) sidedata system uses flagprocessors to signify the
    presence and store/retrieve sidedata from the raw revlog data. This proved to be
    quite fragile from an exchange perspective and a lot more complex than simply
    having a dedicated space in the new revlog format.
    
    This change does not handle exchange (ironically), so the test for amend - that
    uses a bundle - is broken. This functionality is split into the next patches.
    
    Differential Revision: https://phab.mercurial-scm.org/D9993

--- tests/test-revlog-raw.py
+++ tests/test-revlog-raw.py
@@ -51,10 +51,10 @@ _extheader = b'E\n'
 def readprocessor(self, rawtext):
     # True: the returned text could be used to verify hash
     text = rawtext[len(_extheader) :].replace(b'i', b'1')
-    return text, True, {}
+    return text, True
 
 
-def writeprocessor(self, text, sidedata):
+def writeprocessor(self, text):
     # False: the returned rawtext shouldn't be used to verify hash
     rawtext = _extheader + text.replace(b'1', b'i')
     return rawtext, False
@@ -293,7 +293,7 @@ def writecases(rlog, tr):
 
         # Verify text, rawtext, and rawsize
         if isext:
-            rawtext = writeprocessor(None, text, {})[0]
+            rawtext = writeprocessor(None, text)[0]
         else:
             rawtext = text
         if rlog.rawsize(rev) != len(rawtext):
