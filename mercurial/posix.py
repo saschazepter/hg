1de5ba7fb6ef9643386d4593e603d4e56c3c6f9a
Matt Harbison <matt_harbison@yahoo.com>
2021-03-11 21:21:41 -0500

    typing: fix a suppression directive that was mangled by black formatting
    It looks like black is moving comments needed by pytype out of position, and
    causing some things that should be disabled to be enforced anyway.
    
    Differential Revision: https://phab.mercurial-scm.org/D10165

--- mercurial/posix.py
+++ mercurial/posix.py
@@ -321,9 +321,10 @@ def checklink(path):
                     fullpath = os.path.join(cachedir, target)
                     open(fullpath, b'w').close()
                 except IOError as inst:
-                    if (
-                        inst[0] == errno.EACCES
-                    ):  # pytype: disable=unsupported-operands
+                    # pytype: disable=unsupported-operands
+                    if inst[0] == errno.EACCES:
+                        # pytype: enable=unsupported-operands
+
                         # If we can't write to cachedir, just pretend
                         # that the fs is readonly and by association
                         # that the fs won't support symlinks. This
