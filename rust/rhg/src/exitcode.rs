2e181fa6e8bd09707af6f876cf29b4ef35a12317
Pulkit Goyal <7895pulkit@gmail.com>
2021-03-20 02:03:57 +0530

    rhg: add support for detailed exit code for ConfigParseError
    This patch adds basic support for detailed exit code to rhg with support for
    ConfigParseError.
    
    For now, if parsing the config results in error, we silently fallbacks to
    `false`. The python version in this case emits a traceback.
    
    Differential Revision: https://phab.mercurial-scm.org/D10253
    
    --HG--
    extra : rebase_source : e280b992ddf0f6832d0a4c6dd4ca8a5b6fb2db07

--- rust/rhg/src/exitcode.rs
+++ rust/rhg/src/exitcode.rs
@@ -6,6 +6,9 @@ pub const OK: ExitCode = 0;
 /// Generic abort
 pub const ABORT: ExitCode = 255;
 
+// Abort when there is a config related error
+pub const CONFIG_ERROR_ABORT: ExitCode = 30;
+
 /// Generic something completed but did not succeed
 pub const UNSUCCESSFUL: ExitCode = 1;
 
