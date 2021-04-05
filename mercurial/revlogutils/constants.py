ff25cf6edca665e1bd40cb2edcc91d711990b4df
Pierre-Yves David <pierre-yves.david@octobus.net>
2021-04-05 12:21:12 +0200

    revlog: move the details of revlog "v2" index inside revlog.utils.constants
    the revlog module is quite large and this kind of format information would handy
    for other module. So let us start to gather this information about the format in
    a more appropriate place.
    
    We update various reference to this information to use the new "source of truth"
    in the process.
    
    Differential Revision: https://phab.mercurial-scm.org/D10305

--- mercurial/revlogutils/constants.py
+++ mercurial/revlogutils/constants.py
@@ -57,6 +57,21 @@ INDEX_ENTRY_V0 = struct.Struct(b">4l20s20s20s")
 INDEX_ENTRY_V1 = struct.Struct(b">Qiiiiii20s12x")
 assert INDEX_ENTRY_V1.size == 32 * 2
 
+#  6 bytes: offset
+#  2 bytes: flags
+#  4 bytes: compressed length
+#  4 bytes: uncompressed length
+#  4 bytes: base rev
+#  4 bytes: link rev
+#  4 bytes: parent 1 rev
+#  4 bytes: parent 2 rev
+# 32 bytes: nodeid
+#  8 bytes: sidedata offset
+#  4 bytes: sidedata compressed length
+#  20 bytes: Padding to align to 96 bytes (see RevlogV2Plan wiki page)
+INDEX_ENTRY_V2 = struct.Struct(b">Qiiiiii20s12xQi20x")
+assert INDEX_ENTRY_V2.size == 32 * 3
+
 # revlog index flags
 
 # For historical reasons, revlog's internal flags were exposed via the
