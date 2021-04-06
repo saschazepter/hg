37293361099c34d83fe561510042e6d55ae6b56f
Simon Sapin <simon.sapin@octobus.net>
2021-04-06 15:14:19 +0200

    rust: Move "lookup" a.k.a. "unsure" paths into `DirstateStatus` struct
    Instead of having `status()` returning a tuple of those paths and
    `DirstateStatus`.
    
    Differential Revision: https://phab.mercurial-scm.org/D10494
    
    --HG--
    extra : rebase_source : 0485931dcfb0140cfcd0fef62253a367b772c5b6

--- rust/hg-core/src/operations/dirstate_status.rs
+++ rust/hg-core/src/operations/dirstate_status.rs
@@ -5,17 +5,12 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
 
-use crate::dirstate::status::{build_response, Dispatch, HgPathCow, Status};
+use crate::dirstate::status::{build_response, Dispatch, Status};
 use crate::matchers::Matcher;
 use crate::{DirstateStatus, StatusError};
 
-/// A tuple of the paths that need to be checked in the filelog because it's
-/// ambiguous whether they've changed, and the rest of the already dispatched
-/// files.
-pub type LookupAndStatus<'a> = (Vec<HgPathCow<'a>>, DirstateStatus<'a>);
-
 impl<'a, M: ?Sized + Matcher + Sync> Status<'a, M> {
-    pub(crate) fn run(&self) -> Result<LookupAndStatus<'a>, StatusError> {
+    pub(crate) fn run(&self) -> Result<DirstateStatus<'a>, StatusError> {
         let (traversed_sender, traversed_receiver) =
             crossbeam_channel::unbounded();
 
