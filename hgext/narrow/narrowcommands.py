e3227c1cc967a3f83f77fdbe6d9359c119e2ff0c
Pierre-Yves David <pierre-yves.david@octobus.net>
2021-04-14 11:18:48 +0200

    narrow: use `get_unique_pull_path`
    The narrow's `tracked` command does not support multiple destination, lets use
    the dedicated API then.
    
    Differential Revision: https://phab.mercurial-scm.org/D10404

--- hgext/narrow/narrowcommands.py
+++ hgext/narrow/narrowcommands.py
@@ -593,8 +593,8 @@ def trackedcmd(ui, repo, remotepath=None, *pats, **opts):
         # Find the revisions we have in common with the remote. These will
         # be used for finding local-only changes for narrowing. They will
         # also define the set of revisions to update for widening.
-        remotepath = ui.expandpath(remotepath or b'default')
-        url, branches = urlutil.parseurl(remotepath)
+        r = urlutil.get_unique_pull_path(b'tracked', repo, ui, remotepath)
+        url, branches = r
         ui.status(_(b'comparing with %s\n') % urlutil.hidepassword(url))
         remote = hg.peer(repo, opts, url)
 
