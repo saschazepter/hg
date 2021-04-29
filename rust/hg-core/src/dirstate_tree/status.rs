021d3167a1dc8299ac16ee9588269cf04c086a9c
Simon Sapin <simon.sapin@octobus.net>
2021-04-29 11:32:57 +0200

    dirstate-tree: Use HashMap instead of BTreeMap
    BTreeMap has the advantage of its "natural" iteration order being the one we need
    in the status algorithm. With HashMap however, iteration order is undefined so
    we need to allocate a Vec and sort it explicitly.
    
    Unfortunately many BTreeMap operations are slower than in HashMap, and skipping
    that extra allocation and sort is not enough to compensate.
    
    Switching to HashMap + sort makes `hg status` 17% faster in one test case,
    as measure with hyperfine:
    
    ```
    Benchmark #1: ../hg2/hg status -R $REPO --config=experimental.dirstate-tree.in-memory=1
      Time (mean ± σ):     765.0 ms ±   8.8 ms    [User: 1.352 s, System: 0.747 s]
      Range (min … max):   751.8 ms … 778.7 ms    10 runs
    
    Benchmark #2: ./hg status -R $REPO --config=experimental.dirstate-tree.in-memory=1
      Time (mean ± σ):     651.8 ms ±   9.9 ms    [User: 1.251 s, System: 0.799 s]
      Range (min … max):   642.2 ms … 671.8 ms    10 runs
    
    Summary
      './hg status -R $REPO --config=experimental.dirstate-tree.in-memory=1' ran
        1.17 ± 0.02 times faster than '../hg2/hg status -R $REPO --config=experimental.dirstate-tree.in-memory=1'
    ```
    
    * ./hg is this revision
    * ../hg2/hg is its parent
    * $REPO is an old snapshot of mozilla-central
    
    Differential Revision: https://phab.mercurial-scm.org/D10553
    
    --HG--
    extra : rebase_source : 6e3246a3ba765553d07d9c80bcf65495e9e7039b

--- rust/hg-core/src/dirstate_tree/status.rs
+++ rust/hg-core/src/dirstate_tree/status.rs
@@ -110,11 +110,11 @@ impl<'tree, 'a> StatusCommon<'tree, 'a> {
 
         // `merge_join_by` requires both its input iterators to be sorted:
 
-        //
-        // * `BTreeMap` iterates according to keys’ ordering by definition
-
+        let mut dirstate_nodes: Vec<_> = dirstate_nodes.iter_mut().collect();
         // `sort_unstable_by_key` doesn’t allow keys borrowing from the value:
         // https://github.com/rust-lang/rust/issues/34162
+        dirstate_nodes
+            .sort_unstable_by(|(path1, _), (path2, _)| path1.cmp(path2));
         fs_entries.sort_unstable_by(|e1, e2| e1.base_name.cmp(&e2.base_name));
 
         itertools::merge_join_by(
