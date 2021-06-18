17b8b9cdce68d08d1e1bd22e30e8125979ccbdf3
Martin von Zweigbergk <martinvonz@google.com>
2021-06-18 13:44:05 -0700

    histedit: don't record labels for output that we will discard
    Differential Revision: https://phab.mercurial-scm.org/D10881

--- hgext/histedit.py
+++ hgext/histedit.py
@@ -575,7 +575,7 @@ class histeditaction(object):
         parentctx, but does not commit them."""
         repo = self.repo
         rulectx = repo[self.node]
-        repo.ui.pushbuffer(error=True, labeled=True)
+        repo.ui.pushbuffer(error=True)
         hg.update(repo, self.state.parentctxnode, quietempty=True)
         repo.ui.popbuffer()
         stats = applychanges(repo.ui, repo, rulectx, {})
