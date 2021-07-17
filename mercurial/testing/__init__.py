45b425e9c9fee0a0d35ac6c23d243ad13bafaf1d
Cédric Krier <ced@b2ck.com>
2021-07-17 12:47:07 +0200

    testing: do not stop waiting if timeout is 0 (issue6541)
    Differential Revision: https://phab.mercurial-scm.org/D11102

--- mercurial/testing/__init__.py
+++ mercurial/testing/__init__.py
@@ -27,7 +27,7 @@ def wait_file(path, timeout=10):
     timeout *= _timeout_factor()
     start = time.time()
     while not os.path.exists(path):
-        if time.time() - start > timeout:
+        if timeout and time.time() - start > timeout:
             raise RuntimeError(b"timed out waiting for file: %s" % path)
         time.sleep(0.01)
 
