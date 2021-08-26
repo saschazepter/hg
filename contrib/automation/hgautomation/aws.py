d96199a1d348239c3260b9ea213cb900b30e10ca
Gregory Szorc <gregory.szorc@gmail.com>
2021-08-25 19:55:20 -0700

    automation: use gp3 volume type
    This is a newer volume type. It offers a better baseline of performance
    by default and is ~10% cheaper. It doesn't offer burst credits. But
    its performance is guaranteed, unlike gp2, which had wonky performance
    behavior.
    
    Differential Revision: https://phab.mercurial-scm.org/D12132

--- contrib/automation/hgautomation/aws.py
+++ contrib/automation/hgautomation/aws.py
@@ -904,7 +904,7 @@ def ensure_linux_dev_ami(c: AWSConnection, distro='debian10', prefix='hg-'):
                 'Ebs': {
                     'DeleteOnTermination': True,
                     'VolumeSize': 10,
-                    'VolumeType': 'gp2',
+                    'VolumeType': 'gp3',
                 },
             },
         ],
@@ -1048,7 +1048,7 @@ def temporary_linux_dev_instances(
             'Ebs': {
                 'DeleteOnTermination': True,
                 'VolumeSize': 12,
-                'VolumeType': 'gp2',
+                'VolumeType': 'gp3',
             },
         }
     ]
@@ -1075,7 +1075,7 @@ def temporary_linux_dev_instances(
                 'Ebs': {
                     'DeleteOnTermination': True,
                     'VolumeSize': 8,
-                    'VolumeType': 'gp2',
+                    'VolumeType': 'gp3',
                 },
             }
         )
@@ -1151,7 +1151,7 @@ def ensure_windows_dev_ami(
                 'Ebs': {
                     'DeleteOnTermination': True,
                     'VolumeSize': 32,
-                    'VolumeType': 'gp2',
+                    'VolumeType': 'gp3',
                 },
             }
         ],
@@ -1311,7 +1311,7 @@ def temporary_windows_dev_instances(
                 'Ebs': {
                     'DeleteOnTermination': True,
                     'VolumeSize': 32,
-                    'VolumeType': 'gp2',
+                    'VolumeType': 'gp3',
                 },
             }
         ],
