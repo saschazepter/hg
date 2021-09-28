40f0d5a7368cc227aba3a92bff3f1f58f1bcb8ef
Pierre-Yves David <pierre-yves.david@octobus.net>
2021-09-28 18:29:57 +0200

    dirstate: drop unused condition in `from_p2`
    This conditional was added (by me) tentatively because "it seemed more
    correct", but it is not used anywhere yet, and it is missing from the C and the
    Rust implementation. So it seems more consistent to drop it for now.
    
    This effectively backout f94cc63df859c
    
    Differential Revision: https://phab.mercurial-scm.org/D11510
    
    --HG--
    extra : _rewrite_noise : e3d7a4071890a42587c5054325fc50d61e620151
    extra : rebase_source : 8aed2ce6fbeb540253fcfe2ea1f450b40f69e208

--- mercurial/pure/parsers.py
+++ mercurial/pure/parsers.py
@@ -336,7 +336,7 @@ class DirstateItem(object):
         """
         if not self._wc_tracked:
             return False
-        return self._clean_p2 or (not self._p1_tracked and self._p2_tracked)
+        return self._clean_p2
 
     @property
     def from_p2_removed(self):
