88c25f35b74de8fc91ce88391a562fa187a1227a
Pierre-Yves David <pierre-yves.david@octobus.net>
2021-11-29 13:09:04 +0100

    extensions: highlight the name of the faulty extensions in the error message
    This make it easier to understand the message when the extensions name is
    common.
    
    Differential Revision: https://phab.mercurial-scm.org/D11821
    
    --HG--
    extra : _rewrite_noise : 5e8f126774b0ea0e834fc7ef0b477738b62d1564
    extra : rebase_source : 1d2c6979799e92cfb683a087cb3f42515534c5a2

--- mercurial/extensions.py
+++ mercurial/extensions.py
@@ -307,10 +307,12 @@ def loadall(ui, whitelist=None):
             except Exception as inst:
                 msg = stringutil.forcebytestr(inst)
                 if path:
-                    error_msg = _(b"failed to import extension %s from %s: %s")
+                    error_msg = _(
+                        b'failed to import extension "%s" from %s: %s'
+                    )
                     error_msg %= (name, path, msg)
                 else:
-                    error_msg = _(b"failed to import extension %s: %s")
+                    error_msg = _(b'failed to import extension "%s": %s')
                     error_msg %= (name, msg)
                 ui.warn((b"*** %s\n") % error_msg)
                 if isinstance(inst, error.Hint) and inst.hint:
