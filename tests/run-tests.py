cbbfa7a05ee0471227dcf411f783a51da2ab6012
Gregory Szorc <gregory.szorc@gmail.com>
2022-02-20 13:19:21 -0700

    tests: collapse elif PYTHON3 block
    PYTHON3 is always True now so this logic should be identical as to before.
    
    Differential Revision: https://phab.mercurial-scm.org/D12236

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -278,12 +278,11 @@ def checkportisavailable(port):
     except socket.error as exc:
         if WINDOWS and exc.errno == errno.WSAEACCES:
             return False
-        elif PYTHON3:
-            # TODO: make a proper exception handler after dropping py2.  This
-            #       works because socket.error is an alias for OSError on py3,
-            #       which is also the baseclass of PermissionError.
-            if isinstance(exc, PermissionError):
-                return False
+        # TODO: make a proper exception handler after dropping py2.  This
+        #       works because socket.error is an alias for OSError on py3,
+        #       which is also the baseclass of PermissionError.
+        elif isinstance(exc, PermissionError):
+            return False
         if exc.errno not in (
             errno.EADDRINUSE,
             errno.EADDRNOTAVAIL,
