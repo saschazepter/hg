saschazepter/hg

commit 3be23b39a791e7746c1acb46696964eee39442aa

filemerge: move removal of `.orig` extension on temp file close to context
The place where the `.orig` extension is removed in `_maketempfiles()`
doesn't make it clear that it's the backup path, which is why we have
a comment in the code explaining it. Let's instead move it out of the
function and close to where we get it from `backup.path()`, so that
becomes clear.

Differential Revision: https://phab.mercurial-scm.org/D12190

mercurial/filemerge.py
@@ -746,6 +746,9 @@ def _xmerge(repo, mynode, local, other, base, toolconf, backup):
     localoutputpath = None
     if b"$output" in args:
         localoutputpath = backup.path()
+        # Remove the .orig to make syntax-highlighting more likely.
+        if localoutputpath.endswith(b'.orig'):
+            localoutputpath, ext = os.path.splitext(localoutputpath)
 
     with _maketempfiles(
         fco,
@@ -940,10 +943,6 @@ def _maketempfiles(fco, fca, localpath):
     c = tempfromcontext(b"other", fco)
     d = localpath
     if localpath is not None:
-        # We start off with this being the backup filename, so remove the .orig
-        # to make syntax-highlighting more likely.
-        if d.endswith(b'.orig'):
-            d, _ = os.path.splitext(d)
         f, d = maketempfrompath(b"local", d)
         with open(localpath, b'rb') as src:
             f.write(src.read())

commit 04a6d19cd1193928c3f4a65abc28271324c11a31

tests: demonstrate how conflict markers end up $local *and* $output
When a merge tool is configured to keep conflict markers, they are
supposed to be written to `$local` if `$output` is not mentioned in
the tool's `merge-tools.<tool>.args` config, and in `$output` if it is
mentioned. However, I broke the latter case in D12190.

Differential Revision: https://phab.mercurial-scm.org/D12378

--HG--
branch : stable

tests/test-merge-tools.t
@@ -1213,6 +1213,46 @@ premerge=keep keeps conflict markers in:
   # hg resolve --list
   R f
 
+with premerge=keep and $output in tool args, $local does not have markers:
+
+  $ beforemerge
+  [merge-tools]
+  false.whatever=
+  true.priority=1
+  true.executable=cat
+  # hg update -C 1
+  $ hg merge -r 4 --config merge-tools.true.premerge=keep --config 'merge-tools.true.args=$base $local $other $output'
+  merging f
+  revision 0
+  space
+  <<<<<<< working copy: ef83787e2614 - test: revision 1 (known-bad-output !)
+  revision 1
+  space
+  ======= (known-bad-output !)
+  revision 4 (known-bad-output !)
+  >>>>>>> merge rev:    81448d39c9a0 - test: revision 4 (known-bad-output !)
+  revision 4
+  <<<<<<< working copy: ef83787e2614 - test: revision 1
+  revision 1
+  space
+  =======
+  revision 4
+  >>>>>>> merge rev:    81448d39c9a0 - test: revision 4
+  0 files updated, 1 files merged, 0 files removed, 0 files unresolved
+  (branch merge, don't forget to commit)
+  $ aftermerge
+  # cat f
+  <<<<<<< working copy: ef83787e2614 - test: revision 1
+  revision 1
+  space
+  =======
+  revision 4
+  >>>>>>> merge rev:    81448d39c9a0 - test: revision 4
+  # hg stat
+  M f
+  # hg resolve --list
+  R f
+
 premerge=keep-merge3 keeps conflict markers with base content:
 
   $ beforemerge