39ef1c627360540f44b5133a3d6aca04d02c1316
Valentin Gatien-Baron <vgatien-baron@janestreet.com>
2022-04-01 12:46:58 -0400

    dispatch: fix silly blackbox entries when hg is interrupted
    When hg is interrupted, it creates ui.log like this:
    
    1970/01/01 00:00:00 user @0000000000000000000000000000000000000000 (62488)> killed!
     exited 255 after 1.78 seconds
    
    This is due to a scoping problem: two different uses of the name "msg"
    collide. So rename one of them.
    
    Differential Revision: https://phab.mercurial-scm.org/D12427
    
    --HG--
    branch : stable

--- mercurial/dispatch.py
+++ mercurial/dispatch.py
@@ -287,7 +287,7 @@ def _rundispatch(req):
             ferr.write(inst.format())
             return -1
 
-        msg = _formatargs(req.args)
+        formattedargs = _formatargs(req.args)
         starttime = util.timer()
         ret = 1  # default of Python exit code on unhandled exception
         try:
@@ -326,7 +326,7 @@ def _rundispatch(req):
             req.ui.log(
                 b"commandfinish",
                 b"%s exited %d after %0.2f seconds\n",
-                msg,
+                formattedargs,
                 return_code,
                 duration,
                 return_code=return_code,
