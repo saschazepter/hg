14ff1e582e91150336cccec40fbc022b5cb8bc55
Julien Cristau <jcristau@debian.org>
2022-04-06 22:29:49 +0200

    zeroconf: fix deprecation warning with python 3.10
    threading.condition.notifyAll → threading.condition.notify_all
    
    Differential Revision: https://phab.mercurial-scm.org/D12487
    
    --HG--
    extra : rebase_source : 4b7f6b4016f41e3e34787138194fefa190a42606

--- hgext/zeroconf/Zeroconf.py
+++ hgext/zeroconf/Zeroconf.py
@@ -1459,7 +1459,7 @@ class Zeroconf:
     def notifyAll(self):
         """Notifies all waiting threads"""
         self.condition.acquire()
-        self.condition.notifyAll()
+        self.condition.notify_all()
         self.condition.release()
 
     def getServiceInfo(self, type, name, timeout=3000):
