c3bdf463b1d71a022c70dc6dc5cecd741f15505e
Matt Harbison <matt_harbison@yahoo.com>
2022-07-13 11:30:13 -0400

    typing: suppress a few pyi-errors with more recent pytype
    Not sure what's going on here, but these were flagged with pytype 2022.03.21.
    We can't update to something much more recent, because newer versions complain
    about various `attr` uses.

--- mercurial/hgweb/__init__.py
+++ mercurial/hgweb/__init__.py
@@ -18,11 +18,14 @@ from .. import (
 
 from ..utils import procutil
 
+# pytype: disable=pyi-error
 from . import (
     hgweb_mod,
     hgwebdir_mod,
     server,
 )
+
+# pytype: enable=pyi-error
 
 
 def hgweb(config, name=None, baseui=None):
