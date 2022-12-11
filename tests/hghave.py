73b4830933ada25c2b7a024a5b04d6ca254822e5
Anton Shestakov <av6@dwimlabs.net>
2022-12-11 16:44:50 +0400

    hghave: detect Python 3.10 and 3.11 as well
    Noticed because test-contrib-relnotes.t was skipped.

--- tests/hghave.py
+++ tests/hghave.py
@@ -895,7 +895,7 @@ def has_python3exe():
     py = 'python3'
     if os.name == 'nt':
         py = 'py -3'
-    return matchoutput('%s -V' % py, br'^Python 3.(5|6|7|8|9)')
+    return matchoutput('%s -V' % py, br'^Python 3.(5|6|7|8|9|10|11)')
 
 
 @check("pure", "running with pure Python code")
