2bb8ea3725459efc246c58fe478104154d362138
Matt Harbison <matt_harbison@yahoo.com>
2023-01-02 11:38:20 -0500

    copyright: update to 2023
    --HG--
    branch : stable
    extra : topic : copyright-year

--- mercurial/commands.py
+++ mercurial/commands.py
@@ -7939,7 +7939,7 @@ def version_(ui, **opts):
     )
     license = _(
         b"(see https://mercurial-scm.org for more information)\n"
-        b"\nCopyright (C) 2005-2022 Olivia Mackall and others\n"
+        b"\nCopyright (C) 2005-2023 Olivia Mackall and others\n"
         b"This is free software; see the source for copying conditions. "
         b"There is NO\nwarranty; "
         b"not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n"
