4924ace38e6e79102e433d1d6f3727d389f02c43
Pierre-Yves David <pierre-yves.david@octobus.net>
2023-02-02 17:26:10 +0100

    safehasattr: pass attribute name as string instead of bytes
    This is a step toward replacing `util.safehasattr` usage with plain `hasattr`.
    The builtin function behave poorly in Python2 but this was fixed in Python3.
    
    These change are done one by one as they tend to have a small odd to trigger
    puzzling breackage.

--- mercurial/help.py
+++ mercurial/help.py
@@ -810,7 +810,7 @@ def help_(
         doc = gettext(pycompat.getdoc(entry[0]))
         if not doc:
             doc = _(b"(no help text available)")
-        if util.safehasattr(entry[0], b'definition'):  # aliased command
+        if util.safehasattr(entry[0], 'definition'):  # aliased command
             source = entry[0].source
             if entry[0].definition.startswith(b'!'):  # shell alias
                 doc = _(b'shell alias for: %s\n\n%s\n\ndefined by: %s\n') % (
