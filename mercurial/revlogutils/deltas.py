b02fa051dc36c1b35d3e11f312e94738662527f8
Pierre-Yves David <pierre-yves.david@octobus.net>
2023-06-18 00:04:53 +0200

    delta-find: remove dead code intended to deal with forced delta reuse
    Since the case was dealt with sooner (see XXX), we no longer need to deal with
    it in this part of the code.
    
    --HG--
    extra : rebase_source : 4738f635d422be7bbbb8e83c1d9f14e84e97e7f4
    extra : topic : cleanup-delta-policy

--- mercurial/revlogutils/deltas.py
+++ mercurial/revlogutils/deltas.py
@@ -585,12 +585,14 @@ def is_good_delta_info(revlog, deltainfo, revinfo):
     if deltainfo is None:
         return False
 
-    if (
-        revinfo.cachedelta is not None
-        and deltainfo.base == revinfo.cachedelta[0]
-        and revinfo.cachedelta[2] == DELTA_BASE_REUSE_FORCE
-    ):
-        return True
+    # the DELTA_BASE_REUSE_FORCE case should have been taken care of sooner so
+    # we should never end up asking such question. Adding the assert as a
+    # safe-guard to detect anything that would be fishy in this regard.
+    assert (
+        revinfo.cachedelta is None
+        or revinfo.cachedelta[2] != DELTA_BASE_REUSE_FORCE
+        or not revlog._generaldelta
+    )
 
     # - 'deltainfo.distance' is the distance from the base revision --
     #   bounding it limits the amount of I/O we need to do.
@@ -693,14 +695,14 @@ def _candidategroups(
         yield None
         return
 
-    if (
-        cachedelta is not None
-        and nullrev == cachedelta[0]
-        and cachedelta[2] == DELTA_BASE_REUSE_FORCE
-    ):
-        # instruction are to forcibly do a full snapshot
-        yield None
-        return
+    # the DELTA_BASE_REUSE_FORCE case should have been taken care of sooner so
+    # we should never end up asking such question. Adding the assert as a
+    # safe-guard to detect anything that would be fishy in this regard.
+    assert (
+        cachedelta is None
+        or cachedelta[2] != DELTA_BASE_REUSE_FORCE
+        or not revlog._generaldelta
+    )
 
     deltalength = revlog.length
     deltaparent = revlog.deltaparent
@@ -734,15 +736,6 @@ def _candidategroups(
                 continue
             # filter out revision we tested already
             if rev in tested:
-                continue
-
-            if (
-                cachedelta is not None
-                and rev == cachedelta[0]
-                and cachedelta[2] == DELTA_BASE_REUSE_FORCE
-            ):
-                # instructions are to forcibly consider/use this delta base
-                group.append(rev)
                 continue
 
             # an higher authority deamed the base unworthy (e.g. censored)
