4669e98449567e3ba94191268cf9399bcebfc726
Spencer Baugh <sbaugh@janestreet.com>
2023-08-08 11:50:26 -0400

    rust: improve the type on DirsMultiset::from_manifest
    It could only return an HgPathError, but we didn't express this in the
    type, so we needed some unreachable!()s.  Now that is expressed in the
    type.

--- rust/hg-core/src/dirstate/dirs_multiset.rs
+++ rust/hg-core/src/dirstate/dirs_multiset.rs
@@ -62,7 +62,7 @@ impl DirsMultiset {
     /// Initializes the multiset from a manifest.
     pub fn from_manifest(
         manifest: &[impl AsRef<HgPath>],
-    ) -> Result<Self, DirstateMapError> {
+    ) -> Result<Self, HgPathError> {
         let mut multiset = DirsMultiset {
             inner: FastHashMap::default(),
         };
@@ -80,19 +80,17 @@ impl DirsMultiset {
     pub fn add_path(
         &mut self,
         path: impl AsRef<HgPath>,
-    ) -> Result<(), DirstateMapError> {
+    ) -> Result<(), HgPathError> {
         for subpath in files::find_dirs(path.as_ref()) {
             if subpath.as_bytes().last() == Some(&b'/') {
                 // TODO Remove this once PathAuditor is certified
                 // as the only entrypoint for path data
                 let second_slash_index = subpath.len() - 1;
 
-                return Err(DirstateMapError::InvalidPath(
-                    HgPathError::ConsecutiveSlashes {
-                        bytes: path.as_ref().as_bytes().to_owned(),
-                        second_slash_index,
-                    },
-                ));
+                return Err(HgPathError::ConsecutiveSlashes {
+                    bytes: path.as_ref().as_bytes().to_owned(),
+                    second_slash_index,
+                });
             }
             if let Some(val) = self.inner.get_mut(subpath) {
                 *val += 1;
