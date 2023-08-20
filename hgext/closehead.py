b72f1c3ddd7c49fbfeeeba2c0f6b7587bd695ba2
Matt Harbison <matt_harbison@yahoo.com>
2023-08-20 15:38:35 -0400

    closehead: migrate `opts` to native kwargs
    --HG--
    extra : rebase_source : b5430ca26b05b2aa6da3b5b5004291aed0102bb9
    extra : topic : kwarg-conversion-ext

--- hgext/closehead.py
+++ hgext/closehead.py
@@ -54,8 +54,8 @@ def close_branch(ui, repo, *revs, **opts):
             text=message,
             files=[],
             filectxfn=None,
-            user=opts.get(b'user'),
-            date=opts.get(b'date'),
+            user=opts.get('user'),
+            date=opts.get('date'),
             extra=extra,
         )
         tr = repo.transaction(b'commit')
@@ -64,9 +64,7 @@ def close_branch(ui, repo, *revs, **opts):
         cctx.markcommitted(ret)
         tr.close()
 
-    opts = pycompat.byteskwargs(opts)
-
-    revs += tuple(opts.get(b'rev', []))
+    revs += tuple(opts.get('rev', []))
     revs = logcmdutil.revrange(repo, revs)
 
     if not revs:
@@ -80,7 +78,7 @@ def close_branch(ui, repo, *revs, **opts):
         if rev not in heads:
             raise error.Abort(_(b'revision is not an open head: %d') % rev)
 
-    message = cmdutil.logmessage(ui, opts)
+    message = cmdutil.logmessage(ui, pycompat.byteskwargs(opts))
     if not message:
         raise error.Abort(_(b"no commit message specified with -l or -m"))
     extra = {b'close': b'1'}
