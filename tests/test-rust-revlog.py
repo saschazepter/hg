17510de4e3030ec2123acee481b8e066c88aee8b
Raphaël Gomès <rgomes@octobus.net>
2023-09-18 17:11:11 +0200

    rust-revlog: teach the revlog opening code to read the repo options
    This will become necessary as we start writing revlog data from Rust.
    
    --HG--
    extra : rebase_source : cfc08ed8f5f68aed7262bdff71e88ab49774e765
    extra : topic : rust.index.base-methods

--- tests/test-rust-revlog.py
+++ tests/test-rust-revlog.py
@@ -1,3 +1,4 @@
+import struct
 import unittest
 
 try:
@@ -14,6 +15,8 @@ else:
 
 from mercurial.testing import revlog as revlogtesting
 
+header = struct.unpack(">I", revlogtesting.data_non_inlined[:4])[0]
+
 
 @unittest.skipIf(
     rustext is None,
@@ -22,24 +25,24 @@ from mercurial.testing import revlog as revlogtesting
 class RustRevlogIndexTest(revlogtesting.RevlogBasedTestBase):
     def test_heads(self):
         idx = self.parseindex()
-        rustidx = revlog.MixedIndex(idx, revlogtesting.data_non_inlined)
+        rustidx = revlog.MixedIndex(idx, revlogtesting.data_non_inlined, header)
         self.assertEqual(rustidx.headrevs(), idx.headrevs())
 
     def test_get_cindex(self):
         # drop me once we no longer need the method for shortest node
         idx = self.parseindex()
-        rustidx = revlog.MixedIndex(idx, revlogtesting.data_non_inlined)
+        rustidx = revlog.MixedIndex(idx, revlogtesting.data_non_inlined, header)
         cidx = rustidx.get_cindex()
         self.assertTrue(idx is cidx)
 
     def test_len(self):
         idx = self.parseindex()
-        rustidx = revlog.MixedIndex(idx, revlogtesting.data_non_inlined)
+        rustidx = revlog.MixedIndex(idx, revlogtesting.data_non_inlined, header)
         self.assertEqual(len(rustidx), len(idx))
 
     def test_ancestors(self):
         idx = self.parseindex()
-        rustidx = revlog.MixedIndex(idx, revlogtesting.data_non_inlined)
+        rustidx = revlog.MixedIndex(idx, revlogtesting.data_non_inlined, header)
         lazy = LazyAncestors(rustidx, [3], 0, True)
         # we have two more references to the index:
         # - in its inner iterator for __contains__ and __bool__
