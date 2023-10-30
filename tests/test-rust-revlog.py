08ab5c37be2e6cbf449985c9e820e6329e2cb659
Georges Racinet <georges.racinet@octobus.net>
2023-10-30 21:26:17 +0100

    rust-revlog: bare minimal NodeTree exposition
    The independent `NodeTree` instances needs to be associated to an
    index (for forward-checks of candidates) but do not need to
    encompass all revisions from that index.
    
    This is exactly how it is used in `scmutil.shortesthenodeidprefix`
    and we restrict the implementation to the bare minimum needed there
    and to write convincing tests.
    It would of course be fairly trivial to add more.
    
    --HG--
    extra : rebase_source : 1b113e4139c9c2e6a4ffbcce61d4582da0216f02
    extra : topic : rust.index.base-methods

--- tests/test-rust-revlog.py
+++ tests/test-rust-revlog.py
@@ -1,6 +1,8 @@
 import struct
 import unittest
 
+from mercurial.node import hex
+
 try:
     from mercurial import rustext
 
@@ -55,6 +57,35 @@ class RustRevlogIndexTest(revlogtesting.RevlogBasedTestBase):
 
         # let's check bool for an empty one
         self.assertFalse(LazyAncestors(rustidx, [0], 0, False))
+
+
+@unittest.skipIf(
+    rustext is None,
+    "rustext module revlog relies on is not available",
+)
+class RustRevlogNodeTreeClassTest(revlogtesting.RustRevlogBasedTestBase):
+    def test_standalone_nodetree(self):
+        idx = self.parserustindex()
+        nt = revlog.NodeTree(idx)
+        for i in range(4):
+            nt.insert(i)
+
+        bin_nodes = [entry[7] for entry in idx]
+        hex_nodes = [hex(n) for n in bin_nodes]
+
+        for i, node in enumerate(hex_nodes):
+            self.assertEqual(nt.prefix_rev_lookup(node), i)
+            self.assertEqual(nt.prefix_rev_lookup(node[:5]), i)
+
+        # all 4 revisions in idx (standard data set) have different
+        # first nybbles in their Node IDs,
+        # hence `nt.shortest()` should return 1 for them, except when
+        # the leading nybble is 0 (ambiguity with NULL_NODE)
+        for i, (bin_node, hex_node) in enumerate(zip(bin_nodes, hex_nodes)):
+            shortest = nt.shortest(bin_node)
+            expected = 2 if hex_node[0] == ord('0') else 1
+            self.assertEqual(shortest, expected)
+            self.assertEqual(nt.prefix_rev_lookup(hex_node[:shortest]), i)
 
 
 if __name__ == '__main__':
