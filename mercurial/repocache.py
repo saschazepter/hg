050f7d251ef262b72c71bf1d6bfc15f8e8010f0d
Pierre-Yves David <pierre-yves.david@octobus.net>
2024-02-22 10:58:54 +0100

    phases: change the way we warm the phasecache in repocache
    Same logic as for the previous chngeset. We are about to rename and change the
    method used here.

--- mercurial/repocache.py
+++ mercurial/repocache.py
@@ -119,7 +119,8 @@ def _warmupcache(repo):
     repo.obsstore.children
     for name in obsolete.cachefuncs:
         obsolete.getrevs(repo, name)
-    repo._phasecache.loadphaserevs(repo)
+    # ensure the phase cache is fully initialized
+    repo._phasecache.phase(repo, repo.changelog.tiprev())
 
 
 # TODO: think about proper API of attaching preloaded attributes
