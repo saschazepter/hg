308c615b12ddc728a94568d8825de7340fb76283
Raphaël Gomès <rgomes@octobus.net>
2024-09-30 12:10:35 +0200

    rust-files: also return filenode and flags when listing a revision's files
    This is going to be useful when implementing parts of `update` and makes it
    so we don't have to fetch the manifest and each entry twice.
    
    --HG--
    extra : rebase_source : a8170210790d285ca92ec89bfa20b859268dc7d2
    extra : topic : update-fastpaths

--- rust/hg-core/src/operations/list_tracked_files.rs
+++ rust/hg-core/src/operations/list_tracked_files.rs
@@ -5,6 +5,8 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
 
+use std::num::NonZeroU8;
+
 use crate::errors::HgError;
 use crate::matchers::Matcher;
 use crate::repo::Repo;
@@ -12,6 +14,7 @@ use crate::revlog::manifest::Manifest;
 use crate::revlog::RevlogError;
 use crate::utils::filter_map_results;
 use crate::utils::hg_path::HgPath;
+use crate::Node;
 
 /// List files under Mercurial control at a given revision.
 pub fn list_rev_tracked_files(
@@ -31,12 +34,18 @@ pub struct FilesForRev {
     narrow_matcher: Box<dyn Matcher>,
 }
 
+/// Like [`crate::revlog::manifest::ManifestEntry`], but with the `Node`
+/// already checked.
+pub type ExpandedManifestEntry<'a> = (&'a HgPath, Node, Option<NonZeroU8>);
+
 impl FilesForRev {
-    pub fn iter(&self) -> impl Iterator<Item = Result<&HgPath, HgError>> {
+    pub fn iter(
+        &self,
+    ) -> impl Iterator<Item = Result<ExpandedManifestEntry, HgError>> {
         filter_map_results(self.manifest.iter(), |entry| {
             let path = entry.path;
             Ok(if self.narrow_matcher.matches(path) {
-                Some(path)
+                Some((path, entry.node_id()?, entry.flags))
             } else {
                 None
             })
