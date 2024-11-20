ee3aae2a3ec1acd320d3de9df70e7a85a44732af
Georges Racinet <georges.racinet@cloudcrane.io>
2024-11-20 10:58:19 +0100

    hg-pyo3: bump pyo3 to 0.23
    Comes with significant API changes.
    
    --HG--
    extra : rebase_source : d35f79af1905a59a6454b58e816e060c758a89df
    extra : topic : pyo3.bootstrap

--- rust/hg-pyo3/src/lib.rs
+++ rust/hg-pyo3/src/lib.rs
@@ -1,6 +1,6 @@
 use pyo3::prelude::*;
 
 #[pymodule]
-fn pyo3_rustext(_py: Python<'_>, _m: &PyModule) -> PyResult<()> {
+fn pyo3_rustext(_py: Python<'_>, _m: &Bound<'_, PyModule>) -> PyResult<()> {
     Ok(())
 }
