a12965671c10bc08b50a8d34299500ab1cd316f5
paugier <pierre.augier@univ-grenoble-alpes.fr>
2024-11-23 05:50:43 +0100

    setup_py: simplify ssl check

--- setup.py
+++ setup.py
@@ -18,16 +18,7 @@ def eprint(*args, **kwargs):
 
 import ssl
 
-# ssl.HAS_TLSv1* are preferred to check support but they were added in Python
-# 3.7. Prior to CPython commit 6e8cda91d92da72800d891b2fc2073ecbc134d98
-# (backported to the 3.7 branch), ssl.PROTOCOL_TLSv1_1 / ssl.PROTOCOL_TLSv1_2
-# were defined only if compiled against a OpenSSL version with TLS 1.1 / 1.2
-# support. At the mentioned commit, they were unconditionally defined.
-_notset = object()
-has_tlsv1_2 = getattr(ssl, 'HAS_TLSv1_2', _notset)
-if has_tlsv1_2 is _notset:
-    has_tlsv1_2 = getattr(ssl, 'PROTOCOL_TLSv1_2', _notset) is not _notset
-if not has_tlsv1_2:
+if not ssl.HAS_TLSv1_2:
     error = """
 The `ssl` module does not advertise support for TLS 1.2.
 Please make sure that your Python installation was compiled against an OpenSSL
