eea3ea4bcdd6acb51cbc281de7d6f13b42957fb3
Georges Racinet <georges.racinet@cloudcrane.io>
2024-11-29 23:47:37 +0100

    rust-pyo3: facility for submodule registration, using it for dagop
    It turns out that PyO3 has the exact same problem that we encountered
    long ago with rust-cpython. The only difference is that the value
    of `__name__` is not within the `mercurial` package at this point of
    registration.
    
    We reimplement the solution (equivalent to the suggestions in PyO3
    issue tracker anyway), this time in a generic module to limit the
    amount of boilerplate in subsequent applications.
    
    --HG--
    extra : absorb_source : 19701e0cf43367eb487568cac0c9144d6d299cf8
    extra : rebase_source : de15445c9aeebc1bed4a4358f07b904b29d69c2e
    extra : topic : pyo3.bootstrap

--- rust/hg-pyo3/src/lib.rs
+++ rust/hg-pyo3/src/lib.rs
@@ -1,6 +1,19 @@
 use pyo3::prelude::*;
 
+mod dagops;
+mod util;
+
 #[pymodule]
-fn pyo3_rustext(_py: Python<'_>, _m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn pyo3_rustext(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add(
+        "__doc__",
+        "Mercurial core concepts - Rust implementation exposed via PyO3",
+    )?;
+    // the module's __name__ is pyo3_rustext, not mercurial.pyo3_rustext
+    // (at least at this point).
+    let name: String = m.getattr("__name__")?.extract()?;
+    let dotted_name = format!("mercurial.{}", name);
+
+    m.add_submodule(&dagops::init_module(py, &dotted_name)?)?;
     Ok(())
 }
