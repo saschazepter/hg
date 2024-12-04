2fcd3914e06aefb215bd919617110578c5da127e
Pierre-Yves David <pierre-yves.david@octobus.net>
2024-12-04 17:13:39 +0100

    stream: rename all test hook point one number up
    This leave room for a new hook point earlier to detect some race condition with
    caches.
    
    --HG--
    extra : rebase_source : 61dcac8ddd66f316c106b00216584010a07384d5
    extra : topic : fix-stream-race

--- tests/testlib/ext-stream-clone-steps.py
+++ tests/testlib/ext-stream-clone-steps.py
@@ -2,13 +2,13 @@
 #
 # This extension is used through two environment variable
 #
-# HG_TEST_STREAM_WALKED_FILE_1
+# HG_TEST_STREAM_WALKED_FILE_3
 #
 #   path of a file created by the process generating the streaming clone when
 #   it is done gathering data and is ready to unlock the repository and move
 #   to the streaming of content.
 #
-# HG_TEST_STREAM_WALKED_FILE_2
+# HG_TEST_STREAM_WALKED_FILE_4
 #
 #   path of a file to be manually created to let the process generating the
 #   streaming clone proceed to streaming file content.
@@ -21,24 +21,24 @@ from mercurial import (
 )
 
 
-WALKED_FILE_1 = encoding.environ[b'HG_TEST_STREAM_WALKED_FILE_1']
-WALKED_FILE_2 = encoding.environ[b'HG_TEST_STREAM_WALKED_FILE_2']
+WALKED_FILE_3 = encoding.environ[b'HG_TEST_STREAM_WALKED_FILE_3']
+WALKED_FILE_4 = encoding.environ[b'HG_TEST_STREAM_WALKED_FILE_4']
 
 
-def _test_sync_point_walk_1(orig, repo):
-    testing.write_file(WALKED_FILE_1)
+def _test_sync_point_walk_3(orig, repo):
+    testing.write_file(WALKED_FILE_3)
 
 
-def _test_sync_point_walk_2(orig, repo):
+def _test_sync_point_walk_4(orig, repo):
     assert repo._currentlock(repo._lockref) is None
-    testing.wait_file(WALKED_FILE_2)
+    testing.wait_file(WALKED_FILE_4)
 
 
 def uisetup(ui):
     extensions.wrapfunction(
-        streamclone, '_test_sync_point_walk_1', _test_sync_point_walk_1
+        streamclone, '_test_sync_point_walk_3', _test_sync_point_walk_3
     )
 
     extensions.wrapfunction(
-        streamclone, '_test_sync_point_walk_2', _test_sync_point_walk_2
+        streamclone, '_test_sync_point_walk_4', _test_sync_point_walk_4
     )
