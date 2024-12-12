30c5f088be429ee689af47db0272ddcff23d5669
Pierre-Yves David <pierre-yves.david@octobus.net>
2024-12-12 00:51:26 +0000

    ci: build rust wheel too
    We start with the one we need for the CI first.

--- contrib/build-one-linux-wheel.sh
+++ contrib/build-one-linux-wheel.sh
@@ -6,13 +6,25 @@
 set -eu
 
 if [ $# -lt 2 ]; then
-    echo "usage $0 PYTHONTAG DEST_DIR" >&2
+    echo "usage $0 PYTHONTAG DEST_DIR [FLAVOR]" >&2
     echo "" >&2
     echo 'PYTHONTAG should be of the form "cp310-cp310"' >&2
     exit 64
 fi
 py_tag=$1
 destination_directory=$2
+flavor=${3:-c}
+
+
+flavor_arg=""
+if [[ "${flavor}" == "c" ]]; then
+    :
+elif [[ "$flavor" == "rust" ]]; then
+    flavor_arg="--config-setting=--global-option=--rust"
+else
+    echo "unknown flavor: \"$flavor\""
+    exit 96
+fi
 
 
 tmp_wheel_dir=./tmp-wheelhouse
@@ -20,6 +32,6 @@ tmp_wheel_dir=./tmp-wheelhouse
 if [ -e $tmp_wheel_dir ]; then
     rm -rf $tmp_wheel_dir
 fi
-/opt/python/$py_tag/bin/python -m build --outdir $tmp_wheel_dir
+/opt/python/$py_tag/bin/python -m build --outdir $tmp_wheel_dir $flavor_arg
 # adjust it to make it universal
 auditwheel repair $tmp_wheel_dir/*.whl -w $destination_directory
