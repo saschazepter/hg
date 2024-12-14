c127a2fc198d19fb27127f2c8c500231970e9469
Pierre-Yves David <pierre-yves.david@octobus.net>
2024-12-14 10:40:46 +0000

    filter-traceback: simplify handling
    With all the recent change in Python, the best we need and can do is "ignore all
    line starting with a space after a line starting with "Traceback ".
    
    So let us simplify the script.
    
    --HG--
    extra : rebase_source : 2bc9e83bb1cbb6560fdfc75b4a8ed5d86be07bfc
    extra : topic : filter-traceback

--- tests/filtertraceback.py
+++ tests/filtertraceback.py
@@ -15,37 +15,14 @@ sys.stdout = io.TextIOWrapper(
     line_buffering=sys.stdout.line_buffering,
 )
 
-state = 'none'
+in_tb = False
 
 for line in sys.stdin:
-    if state == 'none':
-        if line.startswith('Traceback '):
-            state = 'tb'
-
-    elif state == 'tb':
-        if line.startswith('  File '):
-            state = 'file'
-            continue
-
-        elif not line.startswith(' '):
-            state = 'none'
-
-        elif not line.replace('^', '').replace('~', '').strip():
-            # PEP 657: Fine-grained error locations in tracebacks
-            #                       ~~~~~~^^^^^^^^^
-            continue
-        elif line.startswith(' '):
-            # Python 3.13 provide de full statement context
-            continue
-
-    elif state == 'file':
-        # Ignore one line after "  File ", but sometimes "File" lines are
-        # contiguous:
-        #      File "<frozen importlib._bootstrap>", line 1007, in _find_and_load
-        #      File "<frozen importlib._bootstrap>", line 986, in _find_and_load_unlocked
-        #      File "<frozen importlib._bootstrap>", line 680, in _load_unlocked
-        if not line.startswith('  File '):
-            state = 'tb'
-        continue
-
-    print(line, end='')
+    do_print = not in_tb
+    if line.startswith('Traceback '):
+        in_tb = True
+    elif not line.startswith(' '):
+        in_tb = False
+        do_print = True
+    if do_print:
+        print(line, end='')
