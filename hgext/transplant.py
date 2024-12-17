ef8e47a3eab8f748ae7bb86c9c97b3aa474b547a
Matt Harbison <matt_harbison@yahoo.com>
2024-12-16 21:30:02 -0500

    transplant: don't leak a file descriptor when filtering changesets
    I suppose the first case isn't fully fixed (i.e. if `os.fdopen()` raises an
    exception), but this handles the majority of the potential exceptions.  Closing
    the second file is especially important on Windows, because `os.unlink()` can't
    delete an open file.

--- hgext/transplant.py
+++ hgext/transplant.py
@@ -295,12 +295,12 @@ class transplanter:
         self.ui.status(_(b'filtering %s\n') % patchfile)
         user, date, msg = (changelog[1], changelog[2], changelog[4])
         fd, headerfile = pycompat.mkstemp(prefix=b'hg-transplant-')
-        fp = os.fdopen(fd, 'wb')
-        fp.write(b"# HG changeset patch\n")
-        fp.write(b"# User %s\n" % user)
-        fp.write(b"# Date %d %d\n" % date)
-        fp.write(msg + b'\n')
-        fp.close()
+
+        with os.fdopen(fd, 'wb') as fp:
+            fp.write(b"# HG changeset patch\n")
+            fp.write(b"# User %s\n" % user)
+            fp.write(b"# Date %d %d\n" % date)
+            fp.write(msg + b'\n')
 
         try:
             self.ui.system(
@@ -318,7 +318,9 @@ class transplanter:
                 errprefix=_(b'filter failed'),
                 blockedtag=b'transplant_filter',
             )
-            user, date, msg = self.parselog(open(headerfile, 'rb'))[1:4]
+
+            with open(headerfile, 'rb') as fp:
+                user, date, msg = self.parselog(fp)[1:4]
         finally:
             os.unlink(headerfile)
 
