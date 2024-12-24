38f0dda0f0d6f35c718cb9caffccca77d3295dc8
Georges Racinet <georges.racinet@cloudcrane.io>
2024-12-24 18:02:11 +0100

    rust-pyo3-revlog: new Python and Rust module
    We anticipate that we will actually need to organize the
    code in several Rust submodules, but it will of course still
    be exposed as a single Python module.
    
    --HG--
    extra : rebase_source : 9b5d551c2a8a195cd7516d9436d3123a69ff4e8b
    extra : topic : pyo3.revlog

--- rust/hg-pyo3/src/lib.rs
+++ rust/hg-pyo3/src/lib.rs
@@ -5,6 +5,7 @@ mod convert_cpython;
 mod dagops;
 mod exceptions;
 mod revision;
+mod revlog;
 mod util;
 
 #[pymodule]
@@ -15,9 +16,11 @@ fn pyo3_rustext(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
         "Mercurial core concepts - Rust implementation exposed via PyO3",
     )?;
     let dotted_name: String = m.getattr("__name__")?.extract()?;
+    env_logger::init();
 
     m.add_submodule(&ancestors::init_module(py, &dotted_name)?)?;
     m.add_submodule(&dagops::init_module(py, &dotted_name)?)?;
+    m.add_submodule(&revlog::init_module(py, &dotted_name)?)?;
     m.add("GraphError", py.get_type::<exceptions::GraphError>())?;
     Ok(())
 }
