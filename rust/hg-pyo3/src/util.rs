79a6acdb08b1b7b3c8bbe2145fdef6562ca46f22
Raphaël Gomès <rgomes@octobus.net>
2025-01-03 15:44:29 +0100

    rust-pyo3: add `PyBytesDeref` util
    This mirrors the one in hg-cpython, explanations inline.
    
    --HG--
    extra : rebase_source : ff74b634d9341b651fbe86615d7d44e82e22780d
    extra : topic : pyo3.revlog

--- rust/hg-pyo3/src/util.rs
+++ rust/hg-pyo3/src/util.rs
@@ -4,6 +4,7 @@ use pyo3::buffer::{Element, PyBuffer};
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use pyo3::types::{PyBytes, PyDict};
+use stable_deref_trait::StableDeref;
 
 /// Create the module, with `__package__` given from parent
 ///
@@ -113,6 +114,62 @@ where
         init(&mut rev_buf).map(|_| rev_buf.finish())
     }
 }
+
+/// Safe abstraction over a `PyBytes` together with the `&[u8]` slice
+/// that borrows it. Implements `Deref<Target = [u8]>`.
+///
+/// Calling `PyBytes::data` requires a GIL marker but we want to access the
+/// data in a thread that (ideally) does not need to acquire the GIL.
+/// This type allows separating the call an the use.
+///
+/// It also enables using a (wrapped) `PyBytes` in GIL-unaware generic code.
+pub struct PyBytesDeref {
+    #[allow(unused)]
+    keep_alive: Py<PyBytes>,
+
+    /// Borrows the buffer inside `self.keep_alive`,
+    /// but the borrow-checker cannot express self-referential structs.
+    data: *const [u8],
+}
+
+impl PyBytesDeref {
+    pub fn new(py: Python, bytes: Py<PyBytes>) -> Self {
+        Self {
+            data: bytes.as_bytes(py),
+            keep_alive: bytes,
+        }
+    }
+
+    #[allow(unused)]
+    pub fn unwrap(self) -> Py<PyBytes> {
+        self.keep_alive
+    }
+}
+
+impl std::ops::Deref for PyBytesDeref {
+    type Target = [u8];
+
+    fn deref(&self) -> &[u8] {
+        // Safety: the raw pointer is valid as long as the PyBytes is still
+        // alive, and the returned slice borrows `self`.
+        unsafe { &*self.data }
+    }
+}
+
+unsafe impl StableDeref for PyBytesDeref {}
+
+fn require_send<T: Send>() {}
+
+#[allow(unused)]
+fn static_assert_pybytes_is_send() {
+    #[allow(clippy::no_effect)]
+    require_send::<Py<PyBytes>>;
+}
+
+// Safety: `[Py<PyBytes>]` is Send. Raw pointers are not by default,
+// but here sending one to another thread is fine since we ensure it stays
+// valid.
+unsafe impl Send for PyBytesDeref {}
 
 /// Wrapper around a Python-provided buffer into which the revision contents
 /// will be written. Done for speed in order to save a large allocation + copy.
