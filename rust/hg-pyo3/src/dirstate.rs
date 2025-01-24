22a5e41e254747cea0ba5caf9447560327644a27
Georges Racinet <georges.racinet@cloudcrane.io>
2025-01-24 15:58:18 +0100

    rust-pyo3-dirstate: module definition

---
+++ rust/hg-pyo3/src/dirstate.rs
@@ -0,0 +1,23 @@
+// dirstate.rs
+//
+// Copyright 2019 Raphaël Gomès <rgomes@octobus.net>
+//           2025 Georges Racinet <georges.racinet@cloudcrane.io>
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 or any later version.
+
+//! Bindings for the `hg::dirstate` module provided by the
+//! `hg-core` package.
+//!
+//! From Python, this will be seen as `mercurial.pyo3_rustext.dirstate`
+use crate::utils::new_submodule;
+use pyo3::prelude::*;
+
+pub fn init_module<'py>(
+    py: Python<'py>,
+    package: &str,
+) -> PyResult<Bound<'py, PyModule>> {
+    let m = new_submodule(py, package, "dirstate")?;
+    m.add("__doc__", "Dirstate - Rust implementation exposed via PyO3")?;
+    Ok(m)
+}
