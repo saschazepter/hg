c71b4a19af55cb45aa75c482ac893d5c3a433567
Pierre-Yves David <pierre-yves.david@octobus.net>
2025-04-17 21:31:47 +0200

    hghave: use `has_cmd` for `lfs-test-server` detection
    --HG--
    extra : rebase_source : 3dec2acd33c03c9c9575612bb44ff90c617b7c46
    extra : topic : hghave-which

--- tests/hghave.py
+++ tests/hghave.py
@@ -441,13 +441,7 @@ def has_pygit2():
 # https://github.com/git-lfs/lfs-test-server
 @check("lfs-test-server", "git-lfs test server")
 def has_lfsserver():
-    exe = 'lfs-test-server'
-    if has_windows():
-        exe = 'lfs-test-server.exe'
-    return any(
-        os.access(os.path.join(path, exe), os.X_OK)
-        for path in os.environ["PATH"].split(os.pathsep)
-    )
+    return has_cmd('lfs-test-server')
 
 
 @checkvers("git", "git client (with ext::sh support) version >= %s", ('1.9',))
