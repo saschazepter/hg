9ee4e1f17f3245080ced624b3ae54ff6f7e28b64
Matt Harbison <matt_harbison@yahoo.com>
2025-07-01 13:02:20 -0400

    revlog: fix a bytes vs str crash when emitting a deprecation warning
    This originated today in f8fbc8227493.
    
    --HG--
    extra : rebase_source : 3e291df9ffd114b2ec9d35b7800961177b5d3c1c
    extra : topic : lru-typing

--- mercurial/revlog.py
+++ mercurial/revlog.py
@@ -1366,7 +1366,7 @@ class revlog:
         self.target = target
         if writable is None:
             if target[0] == KIND_FILELOG:
-                msg = "filelog need explicit value for `writable` parameter"
+                msg = b"filelog need explicit value for `writable` parameter"
                 util.nouideprecwarn(msg, b'7.1')
             self._writable = True
         else:
