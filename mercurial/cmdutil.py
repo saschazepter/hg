4f391d868ee3a59ff14945f3e366844eef4a436f
Pierre-Yves David <pierre-yves.david@octobus.net>
2025-08-07 00:01:36 +0200

    cycle-breaking: move the command options group to cmd_impls
    We move them to a more neutral module as many people wants to read them and we
    want to avoid that to pull more dependencies than necessaries.
    
    I take this as an opportunity to rename and type the whole things.
    
    --HG--
    extra : absorb_source : b2d5967a6652db1fd0c63f7771e14f685f69c638
    extra : rebase_source : e467d0bad6c9360c13a24d0650b6dd3db8c88f0a
    extra : topic : cycle-breaking.initialize

--- mercurial/cmdutil.py
+++ mercurial/cmdutil.py
@@ -18,6 +18,7 @@ from typing import (
     Any,
     AnyStr,
     BinaryIO,
+    Final,
     Iterable,
     Literal,
     Optional,
@@ -42,6 +43,7 @@ from . import (
     bookmarks,
     bundle2,
     changelog,
+    cmd_impls,
     copies,
     crecord as crecordmod,
     encoding,
@@ -94,282 +96,24 @@ if TYPE_CHECKING:
 
 stringio = util.stringio
 
-# command options common to all commands
-globalopts: list[
-    tuple[
-        bytes,
-        bytes,
-        bool | bytes | None | list,
-        bytes,
-    ]
-    | tuple[
-        bytes,
-        bytes,
-        bool | bytes | None | list,
-        bytes,
-        bytes,
-    ]
-] = [
-    (
-        b'R',
-        b'repository',
-        b'',
-        _(b'repository root directory or name of overlay bundle file'),
-        _(b'REPO'),
-    ),
-    (b'', b'cwd', b'', _(b'change working directory'), _(b'DIR')),
-    (
-        b'y',
-        b'noninteractive',
-        None,
-        _(
-            b'do not prompt, automatically pick the first choice for all prompts'
-        ),
-    ),
-    (b'q', b'quiet', None, _(b'suppress output')),
-    (b'v', b'verbose', None, _(b'enable additional output')),
-    (
-        b'',
-        b'color',
-        b'',
-        # i18n: 'always', 'auto', 'never', and 'debug' are keywords
-        # and should not be translated
-        _(b"when to colorize (boolean, always, auto, never, or debug)"),
-        _(b'TYPE'),
-    ),
-    (
-        b'',
-        b'config',
-        [],
-        _(b'set/override config option (use \'section.name=value\')'),
-        _(b'CONFIG'),
-    ),
-    (
-        b'',
-        b'config-file',
-        [],
-        _(b'load config file to set/override config options'),
-        _(b'HGRC'),
-    ),
-    (b'', b'debug', None, _(b'enable debugging output')),
-    (b'', b'debugger', None, _(b'start debugger')),
-    (
-        b'',
-        b'encoding',
-        encoding.encoding,
-        _(b'set the charset encoding'),
-        _(b'ENCODE'),
-    ),
-    (
-        b'',
-        b'encodingmode',
-        encoding.encodingmode,
-        _(b'set the charset encoding mode'),
-        _(b'MODE'),
-    ),
-    (b'', b'traceback', None, _(b'always print a traceback on exception')),
-    (b'', b'time', None, _(b'time how long the command takes')),
-    (b'', b'profile', None, _(b'print command execution profile')),
-    (b'', b'version', None, _(b'output version information and exit')),
-    (b'h', b'help', None, _(b'display help and exit')),
-    (b'', b'hidden', False, _(b'consider hidden changesets')),
-    (
-        b'',
-        b'pager',
-        b'auto',
-        _(b"when to paginate (boolean, always, auto, or never)"),
-        _(b'TYPE'),
-    ),
-]
-
-# templates of common command options
-
-dryrunopts = [
-    (b'n', b'dry-run', None, _(b'do not perform actions, just print output')),
-]
-
-confirmopts = [
-    (b'', b'confirm', None, _(b'ask before applying actions')),
-]
-
-remoteopts = [
-    (b'e', b'ssh', b'', _(b'specify ssh command to use'), _(b'CMD')),
-    (
-        b'',
-        b'remotecmd',
-        b'',
-        _(b'specify hg command to run on the remote side'),
-        _(b'CMD'),
-    ),
-    (
-        b'',
-        b'insecure',
-        None,
-        _(b'do not verify server certificate (ignoring web.cacerts config)'),
-    ),
-]
-
-walkopts = [
-    (
-        b'I',
-        b'include',
-        [],
-        _(b'include names matching the given patterns'),
-        _(b'PATTERN'),
-    ),
-    (
-        b'X',
-        b'exclude',
-        [],
-        _(b'exclude names matching the given patterns'),
-        _(b'PATTERN'),
-    ),
-]
-
-commitopts = [
-    (b'm', b'message', b'', _(b'use text as commit message'), _(b'TEXT')),
-    (b'l', b'logfile', b'', _(b'read commit message from file'), _(b'FILE')),
-]
-
-commitopts2 = [
-    (
-        b'd',
-        b'date',
-        b'',
-        _(b'record the specified date as commit date'),
-        _(b'DATE'),
-    ),
-    (
-        b'u',
-        b'user',
-        b'',
-        _(b'record the specified user as committer'),
-        _(b'USER'),
-    ),
-]
-
-commitopts3 = [
-    (b'D', b'currentdate', None, _(b'record the current date as commit date')),
-    (b'U', b'currentuser', None, _(b'record the current user as committer')),
-]
-
-formatteropts = [
-    (b'T', b'template', b'', _(b'display with template'), _(b'TEMPLATE')),
-]
-
-templateopts = [
-    (
-        b'',
-        b'style',
-        b'',
-        _(b'display using template map file (DEPRECATED)'),
-        _(b'STYLE'),
-    ),
-    (b'T', b'template', b'', _(b'display with template'), _(b'TEMPLATE')),
-]
-
-logopts = [
-    (b'p', b'patch', None, _(b'show patch')),
-    (b'g', b'git', None, _(b'use git extended diff format')),
-    (b'l', b'limit', b'', _(b'limit number of changes displayed'), _(b'NUM')),
-    (b'M', b'no-merges', None, _(b'do not show merges')),
-    (b'', b'stat', None, _(b'output diffstat-style summary of changes')),
-    (b'G', b'graph', None, _(b"show the revision DAG")),
-] + templateopts
-
-diffopts = [
-    (b'a', b'text', None, _(b'treat all files as text')),
-    (
-        b'g',
-        b'git',
-        None,
-        _(b'use git extended diff format (DEFAULT: diff.git)'),
-    ),
-    (b'', b'binary', None, _(b'generate binary diffs in git mode (default)')),
-    (b'', b'nodates', None, _(b'omit dates from diff headers')),
-]
-
-diffwsopts = [
-    (
-        b'w',
-        b'ignore-all-space',
-        None,
-        _(b'ignore white space when comparing lines'),
-    ),
-    (
-        b'b',
-        b'ignore-space-change',
-        None,
-        _(b'ignore changes in the amount of white space'),
-    ),
-    (
-        b'B',
-        b'ignore-blank-lines',
-        None,
-        _(b'ignore changes whose lines are all blank'),
-    ),
-    (
-        b'Z',
-        b'ignore-space-at-eol',
-        None,
-        _(b'ignore changes in whitespace at EOL'),
-    ),
-]
-
-diffopts2 = (
-    [
-        (b'', b'noprefix', None, _(b'omit a/ and b/ prefixes from filenames')),
-        (
-            b'p',
-            b'show-function',
-            None,
-            _(
-                b'show which function each change is in (DEFAULT: diff.showfunc)'
-            ),
-        ),
-        (b'', b'reverse', None, _(b'produce a diff that undoes the changes')),
-    ]
-    + diffwsopts
-    + [
-        (
-            b'U',
-            b'unified',
-            b'',
-            _(b'number of lines of context to show'),
-            _(b'NUM'),
-        ),
-        (b'', b'stat', None, _(b'output diffstat-style summary of changes')),
-        (
-            b'',
-            b'root',
-            b'',
-            _(b'produce diffs relative to subdirectory'),
-            _(b'DIR'),
-        ),
-    ]
-)
-
-mergetoolopts = [
-    (b't', b'tool', b'', _(b'specify merge tool'), _(b'TOOL')),
-]
-
-similarityopts = [
-    (
-        b's',
-        b'similarity',
-        b'',
-        _(b'guess renamed files by similarity (0<=s<=100)'),
-        _(b'SIMILARITY'),
-    )
-]
-
-subrepoopts = [(b'S', b'subrepos', None, _(b'recurse into subrepositories'))]
-
-debugrevlogopts = [
-    (b'c', b'changelog', False, _(b'open changelog')),
-    (b'm', b'manifest', False, _(b'open manifest')),
-    (b'', b'dir', b'', _(b'open directory manifest')),
-]
+globalopts: Final[cmd_impls.CmdOptionsT] = cmd_impls.global_opts
+dryrunopts: Final[cmd_impls.CmdOptionsT] = cmd_impls.dry_run_opts
+confirmopts: Final[cmd_impls.CmdOptionsT] = cmd_impls.confirm_opts
+remoteopts: Final[cmd_impls.CmdOptionsT] = cmd_impls.remote_opts
+walkopts: Final[cmd_impls.CmdOptionsT] = cmd_impls.walk_opts
+commitopts: Final[cmd_impls.CmdOptionsT] = cmd_impls.commit_opts
+commitopts2: Final[cmd_impls.CmdOptionsT] = cmd_impls.commit_opts2
+commitopts3: Final[cmd_impls.CmdOptionsT] = cmd_impls.commit_opts3
+formatteropts: Final[cmd_impls.CmdOptionsT] = cmd_impls.formatter_opts
+templateopts: Final[cmd_impls.CmdOptionsT] = cmd_impls.template_opts
+logopts: Final[cmd_impls.CmdOptionsT] = cmd_impls.log_opts
+diffopts: Final[cmd_impls.CmdOptionsT] = cmd_impls.diff_opts
+diffwsopts: Final[cmd_impls.CmdOptionsT] = cmd_impls.diff_ws_opts
+diffopts2: Final[cmd_impls.CmdOptionsT] = cmd_impls.diff_opts2
+mergetoolopts: Final[cmd_impls.CmdOptionsT] = cmd_impls.merge_tool_opts
+similarityopts: Final[cmd_impls.CmdOptionsT] = cmd_impls.similarity_opts
+subrepoopts: Final[cmd_impls.CmdOptionsT] = cmd_impls.subrepo_opts
+debugrevlogopts: Final[cmd_impls.CmdOptionsT] = cmd_impls.debug_revlog_opts
 
 # special string such that everything below this line will be ingored in the
 # editor text
