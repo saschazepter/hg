116522754106a29f1e726fb77ec40a13e0b4cb7d
Pierre-Yves David <pierre-yves.david@octobus.net>
2025-08-07 00:02:36 +0200

    cycle-breaking: get the command options from `cmd_impls` in `help`
    Let us use the new API.
    
    --HG--
    extra : absorb_source : 18ff61da3ecf1c8593173a8a64bd04b0d5336886
    extra : rebase_source : 788e9a8dd81f9f07c7eb8687e7dd76da44e08e4f
    extra : topic : cycle-breaking.initialize

--- mercurial/help.py
+++ mercurial/help.py
@@ -23,6 +23,7 @@ from .i18n import (
     gettext,
 )
 from . import (
+    cmd_impls,
     cmdutil,
     encoding,
     error,
@@ -897,7 +898,9 @@ def help_(
 
         if ui.verbose:
             rst.append(
-                optrst(_(b"global options"), cmdutil.globalopts, ui.verbose, ui)
+                optrst(
+                    _(b"global options"), cmd_impls.global_opts, ui.verbose, ui
+                )
             )
 
         if not ui.verbose:
@@ -1004,7 +1007,7 @@ def help_(
             rst.append(
                 b'\n%s\n'
                 % optrst(
-                    _(b"global options"), cmdutil.globalopts, ui.verbose, ui
+                    _(b"global options"), cmd_impls.global_opts, ui.verbose, ui
                 )
             )
             if name == b'shortlist':
