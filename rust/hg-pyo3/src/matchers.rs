01fd268ab88431a8d2be8782eac1580d9a0e6d26
Raphaël Gomès <rgomes@octobus.net>
2025-11-04 17:45:39 +0100

    rust-matchers: add more methods to `PyMatcher`
    We are about to need these methods for more complex matching.
    
    --HG--
    extra : divergence_source_local : 8ad1204a36d62477db2f1a87106293507e2a4743
    extra : divergence_source_other : f7ed45ccb080756ccddf07d5b5ff47a89aefa5b1
    extra : topic : narrow-shape.more

--- rust/hg-pyo3/src/matchers.rs
+++ rust/hg-pyo3/src/matchers.rs
@@ -4,9 +4,12 @@ use hg::matchers::Matcher;
 use hg::utils::hg_path::HgPath;
 use pyo3::pyclass;
 use pyo3::pymethods;
+use pyo3::types::PyBytes;
+use pyo3::types::PyList;
 use pyo3::types::PyModule;
 use pyo3::types::PyModuleMethods;
 use pyo3::Bound;
+use pyo3::Py;
 use pyo3::PyResult;
 use pyo3::Python;
 
@@ -27,6 +30,28 @@ impl PyMatcher {
 impl PyMatcher {
     fn __call__(&self, path: &[u8]) -> PyResult<bool> {
         Ok(self.inner.matches(HgPath::new(path)))
+    }
+
+    fn always(&self) -> bool {
+        self.inner.matches_everything()
+    }
+
+    fn isexact(&self) -> bool {
+        self.inner.is_exact()
+    }
+
+    fn prefix(&self) -> bool {
+        // TODO might be a lie, but this is an optimization, not a guarantee
+        false
+    }
+
+    fn files(&self, py: Python) -> PyResult<Py<PyList>> {
+        let Some(files) = self.inner.file_set() else {
+            return Ok(PyList::empty(py).unbind());
+        };
+
+        let iter = files.iter().map(|f| PyBytes::new(py, f.as_bytes()));
+        Ok(PyList::new(py, iter)?.unbind())
     }
 }
 
