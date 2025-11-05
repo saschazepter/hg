b38f3d20ce2b025802dcc6585d68caca7759879c
Raphaël Gomès <rgomes@octobus.net>
2025-11-05 18:37:31 +0100

    rust: add the core logic to the shape module
    This adds the code logic needed to think about shards and shapes, which will
    help enable a more composable way of doing narrow. High-level docs will
    come in a future series.
    
    --HG--
    extra : rebase_source : 91b7d238938d61a126dcdfe61f3a7f5a7510e892
    extra : topic : narrow-shape.stream-clone

--- rust/hg-core/src/narrow/shape.rs
+++ rust/hg-core/src/narrow/shape.rs
@@ -1,3 +1,6 @@
+//! The core of the logic for narrow shapes, which enable a composable algebra
+//! for slicing a repo's history along its files.
+
 use std::collections::HashSet;
 use std::io::Write;
 use std::path::Path;
@@ -7,9 +10,15 @@ use std::sync::RwLockReadGuard;
 use std::sync::RwLockWriteGuard;
 
 use itertools::Itertools;
+use lazy_static::lazy_static;
+use regex::Regex;
+use serde::Deserialize;
+use serde::Serialize;
 use sha2::Digest;
 use sha2::Sha256;
 
+use crate::errors::HgError;
+use crate::exit_codes;
 use crate::file_patterns::FilePattern;
 use crate::file_patterns::PatternError;
 use crate::file_patterns::PatternSyntax;
@@ -19,8 +28,348 @@ use crate::matchers::Matcher;
 use crate::matchers::NeverMatcher;
 use crate::matchers::PatternMatcher;
 use crate::matchers::UnionMatcher;
+use crate::repo::Repo;
 use crate::utils::hg_path::HgPath;
 use crate::utils::hg_path::HgPathBuf;
+use crate::utils::hg_path::HgPathError;
+use crate::FastHashMap;
+
+lazy_static! {
+    /// Only lower case ASCII alpha num, `-` and `.`
+    static ref SHARD_NAME_REGEX: Regex =
+        Regex::new(r"^[-\.[[:lower:]][[:digit:]]]+$").unwrap();
+}
+
+/// Canonical name within a repo for a [`Shard`].
+/// Is restricted in which bytes can be used, see [`Self::new`].
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ShardName(String);
+
+impl std::fmt::Display for ShardName {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl ShardName {
+    fn new(name: String) -> Result<Self, HgError> {
+        if !SHARD_NAME_REGEX.is_match(&name) {
+            let msg = format!(
+                "shard name '{}' is invalid: \
+                only lowercase alphanumeric ascii, `-` and `.` are supported.",
+                &name
+            );
+            return Err(HgError::abort_simple(msg));
+        }
+        Ok(Self(name))
+    }
+}
+
+/// Defines a subset of files in this repo's history.
+/// Shards in a repo's [`ShardSet`] are mutually exclusive.
+///
+/// This is a "config-facing" or "admin-facing" concept and should not be
+/// apparent to most users.
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Shard {
+    /// The canonical name for this shard for a given repo
+    name: ShardName,
+    /// The repo paths that this shard covers across the history of its repo
+    paths: Vec<HgPathBuf>,
+    /// Other shards that this shard depends on
+    requires: Vec<ShardName>,
+    /// Whether this shard can be used within or as a [`Shape`]
+    shape: bool,
+}
+
+/// The set of all [`Shard`] that make up a repository.
+///
+/// TODO make this self-referencing if all the cloning ends up being expensive
+#[derive(Debug)]
+pub struct ShardSet {
+    /// All [`Shard`] in the current repo, mapped by their [`ShardName`]
+    shards: FastHashMap<ShardName, Shard>,
+    /// All paths explicitly declared by the shards, mapped to their shard
+    path_to_shard: FastHashMap<HgPathBuf, Shard>,
+}
+
+impl ShardSet {
+    pub fn from_repo_config(repo: &Repo) -> Result<Self, HgError> {
+        let ShapesConfig { version, mut shards } =
+            match repo.store_vfs().try_read("server-shapes")? {
+                Some(data) => toml::from_slice(&data).map_err(|e| {
+                    HgError::abort(
+                        e.to_string(),
+                        exit_codes::CONFIG_PARSE_ERROR_ABORT,
+                        None,
+                    )
+                })?,
+                None => ShapesConfig::default(),
+            };
+        if version != 0 {
+            return Err(HgError::abort(
+                format!("unknown server-shapes version {}", version),
+                exit_codes::CONFIG_ERROR_ABORT,
+                None,
+            ));
+        }
+        let shards_len_before = shards.len();
+        shards.push(ShardConfig {
+            name: "base".to_string(),
+            paths: Some(vec!["".to_string()]),
+            shape: true,
+            requires: None,
+        });
+        shards.push(ShardConfig {
+            name: "full".to_string(),
+            paths: None,
+            shape: true,
+            requires: Some(
+                shards
+                    .iter()
+                    .map(|shard| shard.name.to_owned())
+                    .sorted()
+                    .collect(),
+            ),
+        });
+        let reserved_range = shards_len_before..shards.len();
+        let mut path_to_shard = FastHashMap::default();
+        let mut validated_shards = FastHashMap::default();
+        shards.into_iter().enumerate().try_for_each(|(idx, config)| {
+            Self::validate_shard(
+                config,
+                !reserved_range.contains(&idx),
+                &mut path_to_shard,
+                &mut validated_shards,
+            )
+        })?;
+        Ok(Self { shards: validated_shards, path_to_shard })
+    }
+
+    /// Make sure the shard config is valid, create the shard struct and add
+    /// to the path -> shard mapping
+    fn validate_shard(
+        shard_config: ShardConfig,
+        user_provided: bool,
+        path_to_shard: &mut FastHashMap<HgPathBuf, Shard>,
+        shards: &mut FastHashMap<ShardName, Shard>,
+    ) -> Result<(), HgError> {
+        let ShardConfig { name, shape, paths, requires } = shard_config;
+        if user_provided && matches!(name.as_str(), "base" | "full") {
+            let msg = format!("shard name '{}' is reserved", &name);
+            return Err(HgError::abort(
+                msg,
+                exit_codes::CONFIG_ERROR_ABORT,
+                None,
+            ));
+        }
+
+        if paths.is_none() && requires.is_none() {
+            let msg =
+                format!("shard '{}' needs one of `paths` or `requires`", &name);
+            return Err(HgError::abort(
+                msg,
+                exit_codes::CONFIG_ERROR_ABORT,
+                None,
+            ));
+        }
+
+        let mut hg_paths = vec![];
+        if let Some(config_paths) = paths {
+            for path in config_paths {
+                let on_err = |e| {
+                    let msg = format!(
+                        "invalid path {} in server-shapes: {}",
+                        &path,
+                        HgPathError::from(e)
+                    );
+                    HgError::abort(msg, exit_codes::CONFIG_ERROR_ABORT, None)
+                };
+                let hg_path = HgPathBuf::from_bytes(
+                    path.as_bytes()
+                        .strip_suffix(b"/")
+                        .unwrap_or(path.as_bytes()),
+                );
+                hg_path.check_state().map_err(on_err)?;
+                hg_paths.push(hg_path);
+            }
+        }
+
+        let mut requires_names = vec![];
+        if let Some(requirements) = requires {
+            for requirement in requirements {
+                let name = ShardName::new(requirement)?;
+                requires_names.push(name);
+            }
+        }
+        requires_names.sort();
+
+        let name = ShardName::new(name)?;
+        let shard = Shard {
+            name: name.to_owned(),
+            paths: hg_paths.to_owned(),
+            requires: requires_names,
+            shape,
+        };
+        for path in hg_paths {
+            if path_to_shard.insert(path.to_owned(), shard.to_owned()).is_some()
+            {
+                return Err(HgError::abort(
+                    format!(
+                        "path '{}' is in two separate shards",
+                        String::from_utf8_lossy(path.as_bytes())
+                    ),
+                    exit_codes::CONFIG_ERROR_ABORT,
+                    None,
+                ));
+            }
+        }
+
+        if shards.insert(name.to_owned(), shard).is_some() {
+            return Err(HgError::abort(
+                format!("shard '{}' defined twice", name),
+                exit_codes::CONFIG_ERROR_ABORT,
+                None,
+            ));
+        }
+        Ok(())
+    }
+
+    /// Return all user-facing [`Shard`] as [`Shape`]
+    pub fn all_shapes(&self) -> Result<Vec<Shape>, HgError> {
+        let mut all: Vec<_> =
+            self.shards.iter().filter(|(_, shard)| shard.shape).collect();
+        all.sort();
+        all.into_iter()
+            .map(|(name, shard)| Shape::new(name.to_owned(), self, &[shard]))
+            .collect()
+    }
+
+    /// Return the [`Shape`] of name `name`, or `None`.
+    pub fn shape(&self, name: &str) -> Result<Option<Shape>, HgError> {
+        let shard_name = ShardName::new(name.to_string())?;
+        if let Some(shard) = self.shards.get(&shard_name) {
+            if shard.shape {
+                let shape = Shape::new(shard_name, self, &[shard])?;
+                return Ok(Some(shape));
+            }
+        }
+        Ok(None)
+    }
+
+    fn dependencies<'a>(
+        &'a self,
+        shard: &'a Shard,
+    ) -> Result<FastHashMap<&'a ShardName, &'a Shard>, HgError> {
+        // TODO cache dependencies?
+        let mut all = FastHashMap::default();
+
+        for name in &shard.requires {
+            let mut to_visit = vec![name];
+            let mut visited = HashSet::new();
+            visited.insert(name);
+
+            while let Some(name) = to_visit.pop() {
+                let current_shard = &self.shards[name];
+
+                for direct_dependency in &current_shard.requires {
+                    let new_visit = visited.insert(direct_dependency);
+                    if !new_visit {
+                        return Err(HgError::abort(
+                            format!(
+                                "shard '{}' creates a cycle with '{}'",
+                                name, direct_dependency
+                            ),
+                            exit_codes::CONFIG_ERROR_ABORT,
+                            None,
+                        ));
+                    };
+
+                    to_visit.push(direct_dependency);
+                }
+            }
+            all.extend(
+                visited.into_iter().map(|name| (name, &self.shards[name])),
+            );
+        }
+
+        Ok(all)
+    }
+}
+
+/// Represents a named narrow view into the repo's files (at the history level).
+///
+/// This is a user-facing concept.
+pub struct Shape {
+    name: ShardName,
+    tree: ShardTreeNode,
+}
+
+impl Shape {
+    pub fn new(
+        name: ShardName,
+        shard_set: &ShardSet,
+        shards: &[&Shard],
+    ) -> Result<Self, HgError> {
+        let mut expanded_shards = vec![];
+        for shard in shards {
+            expanded_shards.push(*shard);
+            expanded_shards.extend(shard_set.dependencies(shard)?.values())
+        }
+        let tree = ShardTreeNode::from_shards(shard_set, &expanded_shards)?;
+        Ok(Self { name, tree })
+    }
+
+    /// Returns a [`Matcher`] that expresses the constraints of this shape
+    pub fn matcher(&self, root_path: &Path) -> Box<dyn Matcher + Send> {
+        self.tree.matcher(root_path)
+    }
+
+    pub fn store_fingerprint(&self) -> [u8; 32] {
+        self.tree.fingerprint()
+    }
+
+    pub fn patterns(&self) -> (Vec<HgPathBuf>, Vec<HgPathBuf>) {
+        let (includes, excludes) = self.tree.flat();
+        (
+            includes.into_iter().map(|i| i.to_hg_path_buf()).collect(),
+            excludes.into_iter().map(|i| i.to_hg_path_buf()).collect(),
+        )
+    }
+
+    pub fn name(&self) -> &ShardName {
+        &self.name
+    }
+}
+
+/// Matches the os-disk config for a single shard inside the [`ShapesConfig`]
+#[derive(Serialize, Deserialize)]
+struct ShardConfig {
+    /// Canonical name for this shard. Used both in UI and protocol
+    name: String,
+    #[serde(default)]
+    /// Whether this shard is also a user-accessible shape
+    shape: bool,
+    /// The paths that this shard concerns. Can only be none if this shard
+    /// has [`Self::requires`]
+    paths: Option<Vec<String>>,
+
+    // TODO encoded paths for non-utf8 paths
+    // encoded_paths: Option<Vec<String>>,
+    /// Names of the shards that this shard depends on, to build a hierarchy
+    requires: Option<Vec<String>>,
+}
+
+/// Matches the on-disk format for admins to define their repo's shards and
+/// shapes
+#[derive(Serialize, Deserialize, Default)]
+pub struct ShapesConfig {
+    // XXX should there be a `[meta]` section?
+    /// The version of this config. For now only version `0` exists.
+    version: u32,
+    /// The shards and shapes defined by the repo's admins.
+    shards: Vec<ShardConfig>,
+}
 
 /// A node within a tree of narrow patterns.
 ///
@@ -38,33 +387,60 @@ pub struct ShardTreeNode {
 }
 
 impl ShardTreeNode {
-    pub fn from_patterns(
-        includes: &[FilePattern],
-        excludes: &[FilePattern],
-    ) -> Result<Self, PatternError> {
-        let check_pattern = |pattern: &FilePattern| {
+    /// Create the tree expressed by the server shapes config
+    pub fn from_shards<'a>(
+        shard_set: &'a ShardSet,
+        shards: &[&'a Shard],
+    ) -> Result<Self, HgError> {
+        let mut shard_paths = HashSet::new();
+        // Gather all paths recursively
+        for shard in shards {
+            shard_paths.extend(shard.paths.iter().map(|path| path.as_bytes()));
+            shard_paths.extend(
+                shard_set
+                    .dependencies(shard)?
+                    .values()
+                    .flat_map(|shard| &shard.paths)
+                    .map(|p| p.as_bytes()),
+            );
+        }
+        assert!(!shard_paths.is_empty());
+        Self::from_paths(
+            shard_set.path_to_shard.keys().map(|path| path.as_bytes()),
+            shard_paths,
+        )
+    }
+
+    /// Create the tree expressed by old-style include and exclude patterns
+    pub fn from_patterns<'a>(
+        includes: &'a [FilePattern],
+        excludes: &'a [FilePattern],
+    ) -> Result<Self, HgError> {
+        let check_pattern = |pattern: &'a FilePattern| {
             if pattern.syntax != PatternSyntax::Path {
                 let syntax = format!("{:?}", pattern.syntax);
                 Err(PatternError::UnsupportedSyntax(syntax))
             } else {
-                Ok(ZeroPath::new(&pattern.raw))
+                Ok(pattern.raw.as_slice())
             }
         };
         let mut include_paths = includes
             .iter()
             .map(check_pattern)
-            .collect::<Result<HashSet<_>, _>>()?;
+            .collect::<Result<HashSet<&[u8]>, _>>()
+            .map_err(|e| HgError::abort_simple(e.to_string()))?;
 
         let exclude_paths = excludes
             .iter()
             .map(check_pattern)
-            .collect::<Result<HashSet<_>, _>>()?;
+            .collect::<Result<HashSet<&[u8]>, _>>()
+            .map_err(|e| HgError::abort_simple(e.to_string()))?;
 
-        let empty_path = ZeroPath::new(b"");
+        let empty_path = b"".as_slice();
         // `clone` passes `path:.` by default which is supposed to include
         // everything. This is the wrong API IMO and is a serialization detail
         // hitting internal logic (empty paths are annoying in text formats).
-        let empty_path_alt = ZeroPath::new(b".");
+        let empty_path_alt = b".".as_slice();
 
         let root = if include_paths.contains(&empty_path)
             || include_paths.contains(&empty_path_alt)
@@ -75,12 +451,27 @@ impl ShardTreeNode {
             include_paths.insert(empty_path);
             vec![]
         } else {
-            vec![&empty_path]
+            vec![empty_path]
         };
 
-        let nodes = root
-            .into_iter()
-            .chain(include_paths.iter().chain(exclude_paths.iter()))
+        Self::from_paths(
+            root.into_iter().chain(include_paths.clone()).chain(exclude_paths),
+            include_paths,
+        )
+    }
+
+    /// `paths` is the iterator of all paths (included or not) and `includes`
+    /// contains only the paths that are included.
+    fn from_paths<Paths, Bytes>(
+        paths: Paths,
+        includes: HashSet<&[u8]>,
+    ) -> Result<Self, HgError>
+    where
+        Paths: Iterator<Item = Bytes>,
+        Bytes: AsRef<[u8]>,
+    {
+        // Generate a flat sequence of nodes, sorted via ZeroPath
+        let nodes = paths
             .map(|path| {
                 Arc::new(PanickingRwLock::new(Self {
                     path: ZeroPath::new(path.as_ref()),
@@ -117,6 +508,7 @@ impl ShardTreeNode {
         Ok(root)
     }
 
+    /// Returns a [`Matcher`] that expresses the constraints of this node
     pub fn matcher(&self, root_path: &Path) -> Box<dyn Matcher + Send> {
         let top_matcher = if self.path.is_empty() {
             // We're the root node
