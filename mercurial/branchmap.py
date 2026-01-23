9f6b1a35abbe70841e64c28aacec797f502e507f
Pierre-Yves David <pierre-yves.david@octobus.net>
2026-01-23 15:17:07 +0100

    branchmap: do not overwrite branchmap subset on debugupgradecache
    This avoid creating redundant branchmap on disk.
    
    --HG--
    branch : stable
    extra : rebase_source : 1cd57de24276565a4a2cdf7170ce0982cbc574fe
    extra : topic : bm3-pure-detect

--- mercurial/branchmap.py
+++ mercurial/branchmap.py
@@ -1724,7 +1724,7 @@ class BranchCacheV3(_LocalBranchCache):
             if heads and set(heads).issubset(topo_heads):
                 topo_only.add(branch)
 
-        if topo_only:
+        if topo_only and self._state != STATE_INHERITED:
             self._state = STATE_DIRTY
         self._topo_only_branches = topo_only
         if len(topo_only) == 1:
