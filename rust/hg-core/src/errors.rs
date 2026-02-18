337dd7c00c3da7234a2b6dcca7f06fd0b72d87bc
Raphaël Gomès <rgomes@octobus.net>
2026-02-18 10:48:10 +0100

    rust-errors: refactor `PatternError` display impl to a function
    This will be reused for `Shape` errors in the next change.
    
    --HG--
    extra : rebase_source : c88091707675e062880b17e4e93e721114dd58af
    extra : topic : rust.errors.shape

--- rust/hg-core/src/errors.rs
+++ rust/hg-core/src/errors.rs
@@ -297,72 +297,62 @@ impl fmt::Display for HgError {
                 }
             },
             HgError::Pattern(pattern_error) => {
-                match pattern_error {
-                    PatternError::UnsupportedSyntax(syntax, backtrace) => {
-                        write!(
-                            f,
-                            "{backtrace}unsupported syntax '{}'",
-                            String::from_utf8_lossy(syntax)
-                        )
-                    }
-                    PatternError::IO(error) => error.fmt(f),
-                    PatternError::Path(error) => error.fmt(f),
-                    PatternError::NonRegexPattern(pattern, backtrace) => {
-                        write!(
-                            f,
-                            "{backtrace}'{:?}' cannot be turned into a regex",
-                            pattern
-                        )
-                    }
-                    PatternError::UnclosedGlobAlternation(
-                        pattern,
-                        backtrace,
-                    ) => {
-                        write!(
-                            f,
-                            "{backtrace} unclosed glob alternation \
-                            in pattern '{}'",
-                            String::from_utf8_lossy(pattern)
-                        )
-                    }
-                    PatternError::RegexError { needle, error, backtrace } => {
-                        // The needle is likely already in the error, but let's
-                        // be safe
-                        write!(
-                            f,
-                            "{backtrace}invalid regex '{needle}':\n{error}",
-                        )
-                    }
-                    PatternError::NonUtf8Pattern {
-                        pattern,
-                        valid_up_to,
-                        backtrace,
-                    } => {
-                        write!(
-                            f,
-                            "{backtrace} non-utf8 regex pattern {}, \
-                            valid up to byte {valid_up_to}",
-                            // This is kind of dumb at face value, but it's
-                            // better than
-                            // nothing if we *have* to give a UTF8 output and
-                            // don't
-                            // know the encoding, since we may give *some* info
-                            // about
-                            // what the pattern is.
-                            String::from_utf8_lossy(pattern)
-                        )
-                    }
-                    PatternError::UnsupportedSyntaxNarrow(
-                        syntax,
-                        backtrace,
-                    ) => {
-                        write!(
-                            f,
-                            "{backtrace} unsupported narrow pattern '{syntax:?}'"
-                        )
-                    }
-                }
+                format_pattern_error(f, pattern_error)
             }
+        }
+    }
+}
+
+fn format_pattern_error(
+    f: &mut std::fmt::Formatter<'_>,
+    pattern_error: &PatternError,
+) -> Result<(), std::fmt::Error> {
+    match pattern_error {
+        PatternError::UnsupportedSyntax(syntax, backtrace) => {
+            write!(
+                f,
+                "{backtrace}unsupported syntax '{}'",
+                String::from_utf8_lossy(syntax)
+            )
+        }
+        PatternError::IO(error) => fmt::Display::fmt(&error, f),
+        PatternError::Path(error) => fmt::Display::fmt(&error, f),
+        PatternError::NonRegexPattern(pattern, backtrace) => {
+            write!(
+                f,
+                "{backtrace}'{:?}' cannot be turned into a regex",
+                pattern
+            )
+        }
+        PatternError::UnclosedGlobAlternation(pattern, backtrace) => {
+            write!(
+                f,
+                "{backtrace} unclosed glob alternation in pattern '{}'",
+                String::from_utf8_lossy(pattern)
+            )
+        }
+        PatternError::RegexError { needle, error, backtrace } => {
+            // The needle is likely already in the error, but let's
+            // be safe
+            write!(f, "{backtrace}invalid regex '{needle}':\n{error}",)
+        }
+        PatternError::NonUtf8Pattern { pattern, valid_up_to, backtrace } => {
+            write!(
+                f,
+                "{backtrace} non-utf8 regex pattern {}, \
+                    valid up to byte {valid_up_to}",
+                // This is kind of dumb at face value, but it's
+                // better than
+                // nothing if we *have* to give a UTF8 output and
+                // don't
+                // know the encoding, since we may give *some* info
+                // about
+                // what the pattern is.
+                String::from_utf8_lossy(pattern)
+            )
+        }
+        PatternError::UnsupportedSyntaxNarrow(syntax, backtrace) => {
+            write!(f, "{backtrace} unsupported narrow pattern '{syntax:?}'")
         }
     }
 }
