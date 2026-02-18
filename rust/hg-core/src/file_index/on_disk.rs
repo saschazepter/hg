d32f666b68ef2351bd1833b2bb265f514062c3fd
Raphaël Gomès <rgomes@octobus.net>
2026-02-18 11:35:57 +0100

    rust-errors: use the new backtrace in the fileindex's error display
    We will continue the refactor in the next changes.
    
    --HG--
    extra : rebase_source : 3345cb7e1eddfaf55e5954b5273aca9319e0ffb4
    extra : topic : rust.errors.fileindex

--- rust/hg-core/src/file_index/on_disk.rs
+++ rust/hg-core/src/file_index/on_disk.rs
@@ -57,6 +57,7 @@ impl From<ErrorKind> for Error {
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}corrupted fileindex: ", self.backtrace)?;
         match self.kind {
             ErrorKind::BadFormatMarker => {
                 write!(f, "unrecognized format marker in docket")
