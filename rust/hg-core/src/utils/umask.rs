6bb84d6319181150012250dae0a889d6a47a0694
Arseniy Alekseyev <aalekseyev@janestreet.com>
2026-08-03 16:54:44 -0400

    rust: add an initializer for umask handling
    --HG--
    extra : rebase_source : 415425e9889a0d856cab1c17c7db8792f241c547
    extra : topic : umask-safety

--- rust/hg-core/src/utils/umask.rs
+++ rust/hg-core/src/utils/umask.rs
@@ -3,13 +3,18 @@ use std::sync::OnceLock;
 /// Store the umask for the whole process since it's expensive to get.
 static UMASK: OnceLock<u32> = OnceLock::new();
 
-pub fn get_umask() -> u32 {
-    *UMASK.get_or_init(|| unsafe {
+pub fn initialize() {
+    let _ = UMASK.get_or_init(|| unsafe {
         // TODO is there any way of getting the umask without temporarily
         // setting it? Doesn't this affect all threads in this tiny window?
         let mask = libc::umask(0);
         libc::umask(mask);
         #[allow(clippy::useless_conversion)]
         (mask & 0o777).into()
-    })
+    });
+}
+
+pub fn get_umask() -> u32 {
+    initialize();
+    *UMASK.get().unwrap()
 }
