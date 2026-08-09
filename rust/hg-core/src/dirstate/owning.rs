0c17827f7478852315d9eeff322dceb9c07d925d
Raphaël Gomès <rgomes@octobus.net>
2026-08-09 20:11:18 +0200

    rust-dirstate: switch `OwningDirstateMap` to using `SegmentedBytes`
    For now, this is just passing a contiguous buffer down to the inner
    `DirstateMap`, since there are no multi-extent uses of `SegmentedBytes` yet.
    More changes are to come to get there.
    
    Later down the road, this will unlock the possibility of building the dirstate
    on top of multiple serialization, potentially sharing in-memory serializations
    (like in the FUSE).
    
    --HG--
    extra : absorb_source : 965cf93a4eac14a64803fd9cdac67335bcdf81c7
    extra : rebase_source : e65aedf41a08152eca36201574f57a0a180503c8
    extra : topic : fuse.memory-usage.segmented-dirstate

--- rust/hg-core/src/dirstate/owning.rs
+++ rust/hg-core/src/dirstate/owning.rs
@@ -1,5 +1,3 @@
-use std::ops::Deref;
-
 use self_cell::self_cell;
 use stable_deref_trait::StableDeref;
 
@@ -7,12 +5,13 @@ use super::DirstateError;
 use super::dirstate_map::DirstateIdentity;
 use super::dirstate_map::DirstateMap;
 use crate::DirstateParents;
+use crate::segmented_bytes::SegmentedBytes;
 
 self_cell!(
-    /// Keep a `DirstateMap<'owner>` next to the `owner` buffer that it
+    /// Keep a `DirstateMap<'owner>` next to the `owner` logical buffer that it
     /// borrows.
     pub struct OwningDirstateMap {
-        owner: Box<dyn Deref<Target = [u8]> + Send + Sync>,
+        owner: SegmentedBytes,
         #[covariant]
         dependent: DirstateMap,
     }
@@ -20,7 +19,7 @@ self_cell!(
 
 impl OwningDirstateMap {
     pub fn new_empty(identity: Option<DirstateIdentity>) -> Self {
-        let on_disk = Box::new(&b""[..]);
+        let on_disk = SegmentedBytes::empty();
 
         OwningDirstateMap::new(on_disk, |_| {
             let mut empty = DirstateMap::empty();
@@ -36,11 +35,12 @@ impl OwningDirstateMap {
     where
         OnDisk: StableDeref<Target = [u8]> + Send + Sync + 'static,
     {
-        let on_disk = Box::new(on_disk);
+        let on_disk = SegmentedBytes::from_single_extent(on_disk);
         let mut parents = DirstateParents::NULL;
 
         Ok((
             OwningDirstateMap::try_new(on_disk, |bytes| {
+                let bytes = bytes.as_slice().contiguous_slice(..);
                 DirstateMap::new_v1(bytes, identity).map(|(dmap, p)| {
                     parents = p.unwrap_or(DirstateParents::NULL);
                     dmap
@@ -60,9 +60,10 @@ impl OwningDirstateMap {
     where
         OnDisk: StableDeref<Target = [u8]> + Send + Sync + 'static,
     {
-        let on_disk = Box::new(on_disk);
+        let on_disk = SegmentedBytes::from_single_extent(on_disk);
 
         OwningDirstateMap::try_new(on_disk, |bytes| {
+            let bytes = bytes.as_slice().contiguous_slice(..);
             DirstateMap::new_v2(bytes, data_size, metadata, uuid, identity)
         })
     }
@@ -79,7 +80,7 @@ impl OwningDirstateMap {
     }
 
     pub fn on_disk(&self) -> &[u8] {
-        self.borrow_owner()
+        self.borrow_owner().as_slice().contiguous_slice(..)
     }
 
     pub fn old_uuid(&self) -> Option<&[u8]> {
