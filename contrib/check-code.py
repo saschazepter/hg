37e0010299804e45f3a66f90cb83333cad8511a5
Raphaël Gomès <rgomes@octobus.net>
2026-08-27 17:27:06 +0200

    check-code: make the `sed -i` less dumb
    `sed [...]bundle-group-id` was enough to match the previous rule.
    
    --HG--
    extra : rebase_source : 80987420af90c3b834dfdecb6f78b4d7e1024842
    extra : topic : shapes.sharded-streamclone.pre

--- contrib/check-code.py
+++ contrib/check-code.py
@@ -131,7 +131,7 @@ testpats = [
         (r'\W\$?\(\([^\)\n]*\)\)', "don't use (()) or $(()), use 'expr'"),
         (r'grep.*-q', "don't use 'grep -q', redirect to /dev/null"),
         (r'(?<!hg )grep.* -a', "don't use 'grep -a', use in-line python"),
-        (r'sed.*-i', "don't use 'sed -i', use a temporary file"),
+        (r'sed.* -i', "don't use 'sed -i', use a temporary file"),
         (r'\becho\b.*\\n', "don't use 'echo \\n', use printf"),
         (r'echo -n', "don't use 'echo -n', use printf"),
         (
